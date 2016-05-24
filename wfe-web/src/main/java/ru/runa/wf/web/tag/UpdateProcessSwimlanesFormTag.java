a127ef0030ed1db9b690ff1574b52d40f0f60a22
Igor Yurchenko <beggi_84@mail.ru>
2016-05-24 17:46:24 +0300

    add possibility to update running process swimlane (feature request 195)

---
+++ wfe-web/src/main/java/ru/runa/wf/web/tag/UpdateProcessSwimlanesFormTag.java
@@ -0,0 +1,162 @@
+package ru.runa.wf.web.tag;
+
+import java.util.List;
+
+import org.apache.ecs.html.Div;
+import org.apache.ecs.html.Form;
+import org.apache.ecs.html.Label;
+import org.apache.ecs.html.Option;
+import org.apache.ecs.html.Select;
+import org.apache.ecs.html.TD;
+import org.apache.ecs.html.TR;
+import org.apache.ecs.html.Table;
+import org.tldgen.annotations.Attribute;
+import org.tldgen.annotations.BodyContent;
+
+import ru.runa.common.web.Resources;
+import ru.runa.common.web.tag.TitledFormTag;
+import ru.runa.wf.web.MessagesProcesses;
+import ru.runa.wf.web.action.UpdateProcessSwimlaneAction;
+import ru.runa.wfe.commons.SystemProperties;
+import ru.runa.wfe.execution.dto.WfSwimlane;
+import ru.runa.wfe.presentation.BatchPresentation;
+import ru.runa.wfe.presentation.BatchPresentationFactory;
+import ru.runa.wfe.service.delegate.Delegates;
+import ru.runa.wfe.user.Actor;
+import ru.runa.wfe.user.Executor;
+import ru.runa.wfe.user.Group;
+import ru.runa.wfe.user.User;
+
+@org.tldgen.annotations.Tag(bodyContent = BodyContent.EMPTY, name = "updateProcessSwimlanes")
+public class UpdateProcessSwimlanesFormTag extends TitledFormTag {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long processId;
+
+    @Attribute(required = false, rtexprvalue = true)
+    public void setProcessId(Long id) {
+        processId = id;
+    }
+
+    public Long getProcessId() {
+        return processId;
+    }
+
+    @Override
+    protected void fillFormElement(TD tdFormElement) {
+        User user = getUser();
+        Long processId = getProcessId();
+        List<WfSwimlane> swimlanes = Delegates.getExecutionService().getSwimlanes(user, processId);
+        BatchPresentation batchPresentation = BatchPresentationFactory.EXECUTORS.createNonPaged();
+        batchPresentation.setFieldsToSort(new int[] { 1 }, new boolean[] { true });
+        List<? extends Executor> allowedExecutors = Delegates.getExecutorService().getExecutors(user, batchPresentation);
+        if (!swimlanes.isEmpty()) {
+            if (SystemProperties.isUpdateProcessSwimlanesEnabled() && Delegates.getExecutorService().isAdministrator(user)) {
+                getForm().setEncType(Form.ENC_UPLOAD);
+                String labelTDWidth = "150px";
+
+                Table table = new Table();
+                table.setClass(Resources.CLASS_LIST_TABLE);
+                tdFormElement.addElement(table);
+
+                TR swimlanesComboboxTr = new TR();
+                swimlanesComboboxTr.setClass("swimlane");
+
+                TD labelTd = new TD();
+                Label labelSwimlane = new Label("swimlaneSel");
+                labelSwimlane.addElement(MessagesProcesses.LABEL_SWIMLANE.message(pageContext) + ":&nbsp;");
+                labelTd.addElement(labelSwimlane);
+                labelTd.setWidth(labelTDWidth);
+                swimlanesComboboxTr.addElement(labelTd.setClass(Resources.CLASS_LIST_TABLE_TD));
+
+                Select swimlaneSelect = new Select("swimlaneSelect");
+                swimlaneSelect.setID("swimlaneSelect");
+                for (WfSwimlane swimlane : swimlanes) {
+                    Option option = new Option();
+                    option.setValue(swimlane.getDefinition().getName());
+                    option.addElement(swimlane.getDefinition().getName());
+                    if (swimlane.equals(swimlanes.get(0))) {
+                        option.setSelected(true);
+                    }
+                    swimlaneSelect.addElement(option);
+                }
+
+                TD selectTd = new TD();
+                selectTd.addElement(swimlaneSelect);
+                swimlanesComboboxTr.addElement(selectTd.setClass(Resources.CLASS_LIST_TABLE_TD));
+
+                table.addElement(swimlanesComboboxTr);
+
+                TR currentExecutorTr = new TR();
+
+                labelTd = new TD();
+                Label labelCurrentExecutor = new Label("labelCurrentExecutor");
+                labelCurrentExecutor.addElement(MessagesProcesses.LABEL_SWIMLANE_ASSIGNMENT.message(pageContext) + ":&nbsp;");
+                labelTd.addElement(labelCurrentExecutor);
+                labelTd.setWidth(labelTDWidth);
+                currentExecutorTr.addElement(labelTd.setClass(Resources.CLASS_LIST_TABLE_TD));
+
+                TD currentExecutorTd = new TD();
+                Div currentExecutorDiv = new Div();
+                currentExecutorDiv.setID("currentExecutor");
+                currentExecutorTd.addElement(currentExecutorDiv);
+                currentExecutorTr.addElement(currentExecutorTd.setClass(Resources.CLASS_LIST_TABLE_TD));
+
+                table.addElement(currentExecutorTr);
+
+                TR newExecutorTr = new TR();
+                newExecutorTr.setClass("newExecutor");
+
+                labelTd = new TD();
+                Label labelInputNewExecutor = new Label("newExecutorLabel");
+                labelInputNewExecutor.addElement(MessagesProcesses.LABEL_SWIMLANE_NEW_EXECUTOR.message(pageContext) + ":&nbsp;");
+                labelTd.addElement(labelInputNewExecutor);
+                labelTd.setWidth(labelTDWidth);
+                newExecutorTr.addElement(labelTd.setClass(Resources.CLASS_LIST_TABLE_TD));
+
+                TD inputTd = new TD();
+                Select newExecutorSelect = new Select("newExecutorSelect");
+                newExecutorSelect.setID("newExecutorSelect");
+                for (Executor executor : allowedExecutors) {
+                    Option option = new Option();
+                    option.setValue(executor.getId().intValue());
+                    if (executor instanceof Actor) {
+                        option.addElement(executor.getFullName() != null && !executor.getFullName().isEmpty() ? executor.getFullName() : executor
+                                .getName());
+                    } else if (executor instanceof Group) {
+                        option.addElement(executor.getName() != null && !executor.getName().isEmpty() ? executor.getName() : executor.getFullName());
+                    }
+                    if (executor.equals(allowedExecutors.get(0))) {
+                        option.setSelected(true);
+                    }
+                    newExecutorSelect.addElement(option);
+                }
+                inputTd.addElement(newExecutorSelect);
+
+                newExecutorTr.addElement(inputTd.setClass(Resources.CLASS_LIST_TABLE_TD));
+                table.addElement(newExecutorTr);
+            }
+        } else {
+            Label variablesExist = new Label("swimlanes");
+            variablesExist.addElement(MessagesProcesses.LABEL_NO_SWIMLANES.message(pageContext) + "&nbsp;");
+            tdFormElement.addElement(variablesExist);
+        }
+    }
+
+    @Override
+    protected String getTitle() {
+        return MessagesProcesses.BUTTON_UPDATE_SWIMLANE.message(pageContext);
+    }
+
+    @Override
+    public String getAction() {
+        return UpdateProcessSwimlaneAction.ACTION_PATH + "?id=" + processId;
+    }
+
+    @Override
+    protected String getFormButtonName() {
+        return MessagesProcesses.BUTTON_UPDATE_SWIMLANE.message(pageContext);
+    }
+
+}
