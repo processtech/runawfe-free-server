86abe9e84b4130f405603a2fd484160a2ccd611c
vpanarin <panarin.vn@gmail.com>
2016-08-03 08:56:02 +0300

    https://sourceforge.net/p/runawfe/bugs/544/

--- wfe-core/src/main/java/ru/runa/wfe/security/SecuredObjectType.java
+++ wfe-core/src/main/java/ru/runa/wfe/security/SecuredObjectType.java
@@ -15,8 +15,7 @@ import ru.runa.wfe.user.GroupPermission;
 public enum SecuredObjectType {
     NONE(Permission.class), SYSTEM(WorkflowSystemPermission.class), BOTSTATION(BotStationPermission.class), ACTOR(ActorPermission.class), GROUP(
             GroupPermission.class), RELATION(RelationPermission.class), RELATIONGROUP(RelationPermission.class), RELATIONPAIR(
-                    RelationPermission.class), DEFINITION(
-                            DefinitionPermission.class), PROCESS(ProcessPermission.class), REPORT(ReportPermission.class);
+                    RelationPermission.class), DEFINITION(DefinitionPermission.class), PROCESS(ProcessPermission.class), REPORT(ReportPermission.class);
 
     private Class<? extends Permission> permissionClass;
 
