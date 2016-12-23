fa8f6ba942348c8b497e7609ee64d401ee714e51
Egor Litvinenko <e.v.litvinenko.1@gmail.com>
2016-12-23 15:24:09 +0300

    first state

--- wfe-core/src/main/java/ru/runa/wfe/execution/ExecutionContext.java
+++ wfe-core/src/main/java/ru/runa/wfe/execution/ExecutionContext.java
@@ -281,16 +281,11 @@ public class ExecutionContext {
                         .getValue() instanceof UserTypeMap) {
                     ((UserTypeMap) variable.getValue()).merge((UserTypeMap) baseVariable.getValue(), false);
                 } else if (baseVariable != null) {
-                    if (variable != null) {
-                        if (Utils.isNullOrEmpty(variable.getValue()) || Objects.equal(variable.getDefinition().getDefaultValue(), variable.getValue()) || variable
-                                .getValue() instanceof UserTypeMap) {
-                            variable = getVariableUsingBaseProcess(getProcessDefinition(), getProcess(), name, variable);
-                        }
-                        return variable;
+                    if (!Utils.isNullOrEmpty(baseVariable.getValue()) || variable.getValue() == null) {
+                        variable.setValue(baseVariable.getValue());
                     }
-                    if (SystemProperties.isV3CompatibilityMode()) {
-                        Variable<?> dbVariable = variableLoader.get(getProcess(), name);
-                        return new WfVariable(name, dbVariable != null ? dbVariable.getValue() : null);
+                    if (!Utils.isNullOrEmpty(variable.getValue())) {
+                        return variable;
                     }
                 }
                 return getVariableUsingBaseProcess(baseProcessDefinition, baseProcess, name, variable);
