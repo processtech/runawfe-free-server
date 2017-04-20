a8393e6a852e6d19eb50d01f315d581070d86f59
togacure <togacure@mail.ru>
2017-04-21 01:23:19 +0300

    tnms-2965: iterate by numbers (order) keyword

--- wfe-office/src/main/java/ru/runa/wfe/office/doc/IterateBy.java
+++ wfe-office/src/main/java/ru/runa/wfe/office/doc/IterateBy.java
@@ -1,9 +1,12 @@
 package ru.runa.wfe.office.doc;
 
-
 public enum IterateBy {
-    indexes, items, keys, values;
-    
+    indexes,
+    items,
+    keys,
+    values,
+    numbers;
+
     public static IterateBy identifyByString(DocxConfig config, String string) {
         for (IterateBy iterateBy : IterateBy.values()) {
             if (string.startsWith(iterateBy.name())) {
