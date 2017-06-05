be63f3c2d15106f30b3eaddb749be3802f658139
Kuimov Vladimir <Kuimov Vladimir kuimovvg@gmail.com>
2017-06-05 11:19:24 +0300

    issue379

--- wfe-core/src/main/java/ru/runa/wfe/extension/handler/user/GetTaskPerformerHandler.java
+++ wfe-core/src/main/java/ru/runa/wfe/extension/handler/user/GetTaskPerformerHandler.java
@@ -6,7 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import ru.runa.wfe.audit.ProcessLog;
 import ru.runa.wfe.audit.ProcessLogFilter;
-import ru.runa.wfe.audit.SwimlaneAssignLog;
+import ru.runa.wfe.audit.ProcessLogs;
+import ru.runa.wfe.audit.TaskEndLog;
 import ru.runa.wfe.audit.dao.ProcessLogDAO;
 import ru.runa.wfe.audit.presentation.ExecutorNameValue;
 import ru.runa.wfe.extension.handler.CommonParamBasedHandler;
@@ -27,15 +28,14 @@ public class GetTaskPerformerHandler extends CommonParamBasedHandler {
 		ProcessLogFilter filter = new ProcessLogFilter(handlerData.getProcessId());
 		filter.setNodeId(nodeId);
 		List<ProcessLog> processLogs = processLogDAO.getAll(filter);
-		Object result = null;
-		for (ProcessLog processLog : processLogs) {
-			if (processLog instanceof SwimlaneAssignLog) {
-				Object[] patternArguments = ((SwimlaneAssignLog) processLog).getPatternArguments();
-				String name = ((ExecutorNameValue) patternArguments[1]).getName();
-				result = executorDAO.getExecutor(name);
-				break;
-			}
-		}
+		
+		ProcessLogs processLogsHelper = new ProcessLogs();
+		processLogsHelper.addLogs(processLogs, false);
+		TaskEndLog processLog = processLogsHelper.getFirstOrNull(TaskEndLog.class);
+
+		Object[] patternArguments = processLog.getPatternArguments();
+		String name = ((ExecutorNameValue) patternArguments[1]).getName();
+		Object result = executorDAO.getExecutor(name);
 		handlerData.setOutputParam("result", result);
 	}
 
