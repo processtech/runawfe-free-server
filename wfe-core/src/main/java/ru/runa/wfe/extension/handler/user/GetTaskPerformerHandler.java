e058aa63f2cdc357bcad4df8144cf3f305660e8c
Dofs Serge <dofs197@gmail.com>
2017-06-06 09:14:41 +0300

    pr79 code review

--- wfe-core/src/main/java/ru/runa/wfe/extension/handler/user/GetTaskPerformerHandler.java
+++ wfe-core/src/main/java/ru/runa/wfe/extension/handler/user/GetTaskPerformerHandler.java
@@ -1,42 +1,37 @@
 package ru.runa.wfe.extension.handler.user;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
-import ru.runa.wfe.audit.ProcessLog;
+import ru.runa.wfe.InternalApplicationException;
 import ru.runa.wfe.audit.ProcessLogFilter;
 import ru.runa.wfe.audit.ProcessLogs;
 import ru.runa.wfe.audit.TaskEndLog;
 import ru.runa.wfe.audit.dao.ProcessLogDAO;
-import ru.runa.wfe.audit.presentation.ExecutorNameValue;
 import ru.runa.wfe.extension.handler.CommonParamBasedHandler;
 import ru.runa.wfe.extension.handler.HandlerData;
 import ru.runa.wfe.user.dao.ExecutorDAO;
 
 public class GetTaskPerformerHandler extends CommonParamBasedHandler {
 
-	@Autowired
-	private ProcessLogDAO processLogDAO;
+    @Autowired
+    private ProcessLogDAO processLogDAO;
 
-	@Autowired
-	private ExecutorDAO executorDAO;
+    @Autowired
+    private ExecutorDAO executorDAO;
 
-	@Override
-	protected void executeAction(HandlerData handlerData) throws Exception {
-		String nodeId = handlerData.getInputParamValueNotNull(String.class, "nodeId");
-		ProcessLogFilter filter = new ProcessLogFilter(handlerData.getProcessId());
-		filter.setNodeId(nodeId);
-		List<ProcessLog> processLogs = processLogDAO.getAll(filter);
-		
-		ProcessLogs processLogsHelper = new ProcessLogs();
-		processLogsHelper.addLogs(processLogs, false);
-		TaskEndLog processLog = processLogsHelper.getFirstOrNull(TaskEndLog.class);
-
-		Object[] patternArguments = processLog.getPatternArguments();
-		String name = ((ExecutorNameValue) patternArguments[1]).getName();
-		Object result = executorDAO.getExecutor(name);
-		handlerData.setOutputParam("result", result);
-	}
+    @Override
+    protected void executeAction(HandlerData handlerData) throws Exception {
+        String nodeId = handlerData.getInputParamValueNotNull(String.class, "nodeId");
+        ProcessLogFilter filter = new ProcessLogFilter(handlerData.getProcessId());
+        filter.setNodeId(nodeId);
+        ProcessLogs processLogs = new ProcessLogs();
+        processLogs.addLogs(processLogDAO.getAll(filter), false);
+        TaskEndLog taskEndLog = processLogs.getLastOrNull(TaskEndLog.class);
+        if (taskEndLog == null) {
+            throw new InternalApplicationException("No task end log found for node " + nodeId);
+        }
+        Object result = executorDAO.getExecutor(taskEndLog.getActorName());
+        handlerData.setOutputParam("result", result);
+    }
 
 }
