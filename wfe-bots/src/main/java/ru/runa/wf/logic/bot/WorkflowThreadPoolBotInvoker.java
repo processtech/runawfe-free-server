acb0c41fa2e85277cdab0c4a2ad2f46a269cbc0b
dkononov <estetis21@gmail.com>
2017-10-12 09:57:58 +0400

    bugfix-445
    поправил форматирование

--- wfe-bots/src/main/java/ru/runa/wf/logic/bot/WorkflowThreadPoolBotInvoker.java
+++ wfe-bots/src/main/java/ru/runa/wf/logic/bot/WorkflowThreadPoolBotInvoker.java
@@ -32,9 +32,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
-
 import ru.runa.wfe.ConfigurationException;
 import ru.runa.wfe.bot.Bot;
 import ru.runa.wfe.bot.BotStation;
@@ -46,6 +43,9 @@ import ru.runa.wfe.security.AuthenticationException;
 import ru.runa.wfe.service.delegate.Delegates;
 import ru.runa.wfe.task.dto.WfTask;
 import ru.runa.wfe.user.User;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 public class WorkflowThreadPoolBotInvoker implements BotInvoker, Runnable {
     private final Log log = LogFactory.getLog(WorkflowThreadPoolBotInvoker.class);
@@ -224,8 +224,8 @@ public class WorkflowThreadPoolBotInvoker implements BotInvoker, Runnable {
             }
         }
         for (String taskName : sequentialTasks.keySet()) {
-            WorkflowSequentialBotTaskExecutor botTaskExecutor = new WorkflowSequentialBotTaskExecutor(botExecutor.getBot(),
-                    botExecutor.getBotTasks().get(taskName), sequentialTasks.get(taskName));
+            WorkflowSequentialBotTaskExecutor botTaskExecutor = new WorkflowSequentialBotTaskExecutor(botExecutor.getBot(), botExecutor.getBotTasks()
+                    .get(taskName), sequentialTasks.get(taskName));
             ScheduledFuture<?> future = executor.schedule(botTaskExecutor, 200, TimeUnit.MILLISECONDS);
             scheduledTasks.put(botTaskExecutor, future);
         }
