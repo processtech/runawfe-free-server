2748d6ef8f572728f2547f8edf31439e33de25c0
Dofs <dofs197@gmail.com>
2017-10-28 20:17:48 +0300

    MDB redelivery through CMT instead of BMT where possible

--- wfe-service/src/main/java/ru/runa/wfe/service/impl/ReceiveMessageBean.java
+++ wfe-service/src/main/java/ru/runa/wfe/service/impl/ReceiveMessageBean.java
@@ -32,7 +32,6 @@ import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.ObjectMessage;
-import javax.transaction.UserTransaction;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -44,7 +43,6 @@ import ru.runa.wfe.audit.ReceiveMessageLog;
 import ru.runa.wfe.audit.dao.ProcessLogDAO;
 import ru.runa.wfe.commons.Errors;
 import ru.runa.wfe.commons.SystemProperties;
-import ru.runa.wfe.commons.TransactionalExecutor;
 import ru.runa.wfe.commons.Utils;
 import ru.runa.wfe.definition.dao.IProcessDefinitionLoader;
 import ru.runa.wfe.execution.ExecutionContext;
@@ -61,13 +59,12 @@ import ru.runa.wfe.var.IVariableProvider;
 import ru.runa.wfe.var.VariableMapping;
 
 import com.google.common.base.Objects;
-import com.google.common.base.Throwables;
 import com.google.common.collect.Lists;
 
-@MessageDriven(activationConfig = { @ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/bpmMessages"),
-        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
-        @ActivationConfigProperty(propertyName = "useDLQ", propertyValue = "false") })
-@TransactionManagement(TransactionManagementType.BEAN)
+//@ActivationConfigProperty(propertyName = "maxSession", propertyValue = "2")
+@MessageDriven(activationConfig = { @ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/bpmMessages"),//
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue") })
+@TransactionManagement(TransactionManagementType.CONTAINER)
 @Interceptors({ EjbExceptionSupport.class, PerformanceObserver.class, SpringBeanAutowiringInterceptor.class })
 @SuppressWarnings("unchecked")
 public class ReceiveMessageBean implements MessageListener {
@@ -86,12 +83,10 @@ public class ReceiveMessageBean implements MessageListener {
         List<ReceiveMessageData> handlers = Lists.newArrayList();
         ObjectMessage message = (ObjectMessage) jmsMessage;
         String messageString = Utils.toString(message, false);
-        UserTransaction transaction = context.getUserTransaction();
         ErrorEventData errorEventData = null;
         try {
             log.debug("Received " + messageString);
             errorEventData = ErrorEventData.match(message);
-            transaction.begin();
             List<Token> tokens;
             if (SystemProperties.isProcessExecutionMessagePredefinedSelectorEnabled()) {
                 if (SystemProperties.isProcessExecutionMessagePredefinedSelectorOnlyStrictComplianceHandling()) {
@@ -145,59 +140,49 @@ public class ReceiveMessageBean implements MessageListener {
                     log.error("Unable to handle " + token, e);
                 }
             }
-            transaction.commit();
+            if (handlers.isEmpty()) {
+                if (errorEventData != null) {
+                    String errorMessage = "Unexpected errorEvent in processId = " + errorEventData.processId + ", nodeId = " + errorEventData.nodeId;
+                    log.error(errorMessage);
+                    Errors.addSystemError(new InternalApplicationException(errorMessage));
+                } else {
+                    log.info("Rejecting " + messageString);
+                    context.setRollbackOnly();
+                }
+            } else {
+                log.info("Handling " + messageString);
+                for (ReceiveMessageData data : handlers) {
+                    handleMessage(data, message);
+                }
+            }
         } catch (Exception e) {
             log.error("", e);
-            Utils.rollbackTransaction(transaction);
-            Throwables.propagate(e);
-        }
-        if (handlers.isEmpty()) {
-            if (errorEventData != null) {
-                String errorMessage = "Unexpected errorEvent in processId = " + errorEventData.processId + ", nodeId = " + errorEventData.nodeId;
-                log.error(errorMessage);
-                Errors.addSystemError(new InternalApplicationException(errorMessage));
-            } else {
-                throw new MessagePostponedException(messageString);
-            }
-        }
-        for (ReceiveMessageData data : handlers) {
-            handleMessage(data, message);
+            context.setRollbackOnly();
         }
     }
 
-    private void handleMessage(final ReceiveMessageData data, final ObjectMessage message) {
-        try {
-            new TransactionalExecutor(context.getUserTransaction()) {
-
-                @Override
-                protected void doExecuteInTransaction() throws Exception {
-                    log.info("Handling " + message + " for " + data);
-                    Token token = tokenDAO.getNotNull(data.tokenId);
-                    if (!Objects.equal(token.getNodeId(), data.node.getNodeId())) {
-                        throw new InternalApplicationException(token + " not in " + data.node.getNodeId());
-                    }
-                    ProcessDefinition processDefinition = processDefinitionLoader.getDefinition(token.getProcess().getDeployment().getId());
-                    ExecutionContext executionContext = new ExecutionContext(processDefinition, token);
-                    executionContext.activateTokenIfHasPreviousError();
-                    executionContext.addLog(new ReceiveMessageLog(data.node, Utils.toString(message, true)));
-                    Map<String, Object> map = (Map<String, Object>) message.getObject();
-                    for (VariableMapping variableMapping : data.node.getVariableMappings()) {
-                        if (!variableMapping.isPropertySelector()) {
-                            if (map.containsKey(variableMapping.getMappedName())) {
-                                Object value = map.get(variableMapping.getMappedName());
-                                executionContext.setVariableValue(variableMapping.getName(), value);
-                            } else {
-                                log.warn("message does not contain value for '" + variableMapping.getMappedName() + "'");
-                            }
-                        }
-                    }
-                    data.node.leave(executionContext);
-                }
-            }.executeInTransaction(true);
-        } catch (final Throwable th) {
-            Utils.failProcessExecution(context.getUserTransaction(), data.tokenId, th);
-            Throwables.propagate(th);
+    private void handleMessage(final ReceiveMessageData data, final ObjectMessage message) throws JMSException {
+        log.debug("Handling " + message + " for " + data);
+        Token token = tokenDAO.getNotNull(data.tokenId);
+        if (!Objects.equal(token.getNodeId(), data.node.getNodeId())) {
+            throw new InternalApplicationException(token + " not in " + data.node.getNodeId());
         }
+        ProcessDefinition processDefinition = processDefinitionLoader.getDefinition(token.getProcess().getDeployment().getId());
+        ExecutionContext executionContext = new ExecutionContext(processDefinition, token);
+        executionContext.activateTokenIfHasPreviousError();
+        executionContext.addLog(new ReceiveMessageLog(data.node, Utils.toString(message, true)));
+        Map<String, Object> map = (Map<String, Object>) message.getObject();
+        for (VariableMapping variableMapping : data.node.getVariableMappings()) {
+            if (!variableMapping.isPropertySelector()) {
+                if (map.containsKey(variableMapping.getMappedName())) {
+                    Object value = map.get(variableMapping.getMappedName());
+                    executionContext.setVariableValue(variableMapping.getName(), value);
+                } else {
+                    log.warn("message does not contain value for '" + variableMapping.getMappedName() + "'");
+                }
+            }
+        }
+        data.node.leave(executionContext);
     }
 
     private static class ReceiveMessageData {
