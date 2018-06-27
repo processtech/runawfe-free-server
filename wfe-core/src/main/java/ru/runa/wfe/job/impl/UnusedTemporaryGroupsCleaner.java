d369e49775c07e091043fca06cd107d7b4189079
Dofs <dofs197@gmail.com>
2018-06-27 21:48:31 +0300

    delete temporary groups faster

--- wfe-core/src/main/java/ru/runa/wfe/job/impl/UnusedTemporaryGroupsCleaner.java
+++ wfe-core/src/main/java/ru/runa/wfe/job/impl/UnusedTemporaryGroupsCleaner.java
@@ -1,31 +1,45 @@
 package ru.runa.wfe.job.impl;
 
-import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import ru.runa.wfe.execution.dao.ProcessDAO;
-import ru.runa.wfe.user.TemporaryGroup;
-import ru.runa.wfe.user.dao.ExecutorDAO;
-import ru.runa.wfe.user.logic.ExecutorLogic;
 
 public class UnusedTemporaryGroupsCleaner {
     protected final Log log = LogFactory.getLog(getClass());
+    // @Autowired
+    // protected HibernateQueryFactory queryFactory;
     @Autowired
-    private ExecutorLogic executorLogic;
-    @Autowired
-    private ExecutorDAO executorDAO;
-    @Autowired
-    private ProcessDAO processDAO;
+    protected SessionFactory sessionFactory;
 
     @Transactional
     public void execute() {
-        List<TemporaryGroup> groups = executorDAO.getUnusedTemporaryGroups();
-        log.debug("Removing " + groups.size() + " groups");
-        for (TemporaryGroup group : groups) {
-            executorLogic.remove(group);
-        }
+        log.debug("Cleaning unused temporary groups");
+        // TODO seems like bug in generated query: Column "TEMPORARYG0_.PROCESS_ID" not found
+        // QTemporaryGroup tg = QTemporaryGroup.temporaryGroup;
+        // QSwimlane s = QSwimlane.swimlane;
+        // QTask t = QTask.task;
+        // queryFactory
+        // .delete(tg)
+        // .where(tg.processId.notIn(JPAExpressions.selectDistinct(s.process.id).from(s)).and(
+        // tg.processId.notIn(JPAExpressions.selectDistinct(t.process.id).from(t)))).execute();
+        //
+        // This is not working with same error
+        // sessionFactory.getCurrentSession()
+        // .createQuery("delete from TemporaryGroup where processId not in (select process.id from Swimlane union select process.id from Task)")
+        // .executeUpdate();
+        //
+        sessionFactory
+                .getCurrentSession()
+                .createSQLQuery(
+                        "DELETE FROM EXECUTOR_GROUP_MEMBER WHERE GROUP_ID IN (SELECT ID FROM EXECUTOR WHERE DISCRIMINATOR = 'T' AND ID NOT IN (SELECT DISTINCT(EXECUTOR_ID) FROM BPM_SWIMLANE WHERE EXECUTOR_ID IS NOT NULL UNION SELECT DISTINCT(EXECUTOR_ID) FROM BPM_TASK WHERE EXECUTOR_ID IS NOT NULL))")
+                .executeUpdate();
+        sessionFactory
+                .getCurrentSession()
+                .createSQLQuery(
+                        "DELETE FROM EXECUTOR WHERE DISCRIMINATOR = 'T' AND ID NOT IN (SELECT DISTINCT(EXECUTOR_ID) FROM BPM_SWIMLANE WHERE EXECUTOR_ID IS NOT NULL UNION SELECT DISTINCT(EXECUTOR_ID) FROM BPM_TASK WHERE EXECUTOR_ID IS NOT NULL)")
+                .executeUpdate();
     }
 
 }
