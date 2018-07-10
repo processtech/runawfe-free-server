Andreipestov/runawfe-server

rm707: CamelCase notation; no more Hungarian notation

wfe-core/src/test/java/ru/runa/wfe/task/logic/TestCaseDataSet.java
@@ -1,63 +1,60 @@
 package ru.runa.wfe.task.logic;
 
 import java.util.Set;
-
-import ru.runa.wfe.audit.ProcessLog;
-import ru.runa.wfe.audit.dao.IProcessLogDAO;
-import ru.runa.wfe.commons.dao.IGenericDAO;
-import ru.runa.wfe.definition.dao.IProcessDefinitionLoader;
+import ru.runa.wfe.audit.dao.ProcessLogDao;
+import ru.runa.wfe.definition.dao.ProcessDefinitionLoader;
 import ru.runa.wfe.execution.ExecutionContext;
-import ru.runa.wfe.execution.IExecutionContextFactory;
 import ru.runa.wfe.presentation.BatchPresentation;
-import ru.runa.wfe.presentation.hibernate.IBatchPresentationCompilerFactory;
+import ru.runa.wfe.presentation.hibernate.BatchPresentationCompilerFactory;
 import ru.runa.wfe.ss.SubstitutionCriteria;
-import ru.runa.wfe.ss.logic.ISubstitutionLogic;
+import ru.runa.wfe.ss.logic.SubstitutionLogic;
 import ru.runa.wfe.task.Task;
-import ru.runa.wfe.task.dto.IWfTaskFactory;
+import ru.runa.wfe.task.dao.TaskDao;
+import ru.runa.wfe.task.dto.WfTaskFactory;
 import ru.runa.wfe.user.Actor;
 import ru.runa.wfe.user.EscalationGroup;
 import ru.runa.wfe.user.Executor;
-import ru.runa.wfe.user.dao.IExecutorDAO;
+import ru.runa.wfe.user.dao.ExecutorDao;
 
 public abstract class TestCaseDataSet implements ITestCaseDataSet {
 
     @Override
-    public void mockRules(IWfTaskFactory tastFactory) {
+    public void mockRules(WfTaskFactory tastFactory) {
 
     }
 
     @Override
-    public void mockRules(IExecutorDAO executorDAO) {
+    public void mockRules(ExecutorDao executorDao) {
 
     }
 
     @Override
-    public void mockRules(ISubstitutionLogic substitutionLogic) {
+    public void mockRules(SubstitutionLogic substitutionLogic) {
 
     }
 
     @Override
-    public void mockRules(IProcessDefinitionLoader processDefinitionLoader) {
+    public void mockRules(ProcessDefinitionLoader processDefinitionLoader) {
 
     }
 
     @Override
-    public void mockRules(IGenericDAO<Task> taskDAO) {
+    public void mockRules(TaskDao taskDao) {
 
     }
 
     @Override
-    public void mockRules(IExecutionContextFactory exeContextFactory) {
+    public void mockRules(ExecutionContextFactory exeContextFactory) {
 
     }
 
     @Override
-    public void mockRules(IBatchPresentationCompilerFactory<?> batchCompilerFactory) {
+    public void mockRules(BatchPresentationCompilerFactory<?> batchCompilerFactory) {
 
     }
 
     @Override
-    public void mockRules(IProcessLogDAO<ProcessLog> logDAO) {
+    public void mockRules(ProcessLogDao logDAO) {
 
     }
 

wfe-service/src/main/java/ru/runa/wfe/service/ExecutionService.java
@@ -36,7 +36,7 @@ import ru.runa.wfe.user.User;
 import ru.runa.wfe.validation.ValidationException;
 import ru.runa.wfe.var.dto.WfVariable;
 import ru.runa.wfe.var.dto.WfVariableHistoryState;
-import ru.runa.wfe.var.file.FileVariable;
+import ru.runa.wfe.var.file.FileVariableImpl;
 
 /**
  * Process execution service.
@@ -262,10 +262,10 @@ public interface ExecutionService {
      *            process id
      * @param variableName
      *            variable name
-     * @return IFileVariable or <code>null</code>
+     * @return FileVariable or <code>null</code>
      * @throws ProcessDoesNotExistException
      */
-    public FileVariable getFileVariableValue(User user, Long processId, String variableName) throws ProcessDoesNotExistException;
+    public FileVariableImpl getFileVariableValue(User user, Long processId, String variableName) throws ProcessDoesNotExistException;
 
     /**
      * Updates process variables without any signalling.

wfe-service/src/main/java/ru/runa/wfe/service/delegate/ExecutionServiceDelegate.java
@@ -34,12 +34,12 @@ import ru.runa.wfe.user.Executor;
 import ru.runa.wfe.user.User;
 import ru.runa.wfe.var.dto.WfVariable;
 import ru.runa.wfe.var.dto.WfVariableHistoryState;
-import ru.runa.wfe.var.file.FileVariable;
+import ru.runa.wfe.var.file.FileVariableImpl;
 
 /**
  * Created on 28.09.2004
  */
-public class ExecutionServiceDelegate extends EJB3Delegate implements ExecutionService {
+public class ExecutionServiceDelegate extends Ejb3Delegate implements ExecutionService {
 
     public ExecutionServiceDelegate() {
         super(ExecutionService.class);
@@ -176,7 +176,7 @@ public class ExecutionServiceDelegate extends EJB3Delegate implements ExecutionS
     }
 
     @Override
-    public FileVariable getFileVariableValue(User user, Long processId, String variableName) throws ProcessDoesNotExistException {
+    public FileVariableImpl getFileVariableValue(User user, Long processId, String variableName) throws ProcessDoesNotExistException {
         try {
             return getExecutionService().getFileVariableValue(user, processId, variableName);
         } catch (Exception e) {

processtech/runawfe-free-server

wfe-core/src/test/java/ru/runa/wfe/task/logic/TestCaseDataSet.java
@@ -1,63 +1,61 @@
 package ru.runa.wfe.task.logic;
 
 import java.util.Set;
-
-import ru.runa.wfe.audit.ProcessLog;
-import ru.runa.wfe.audit.dao.IProcessLogDAO;
-import ru.runa.wfe.commons.dao.IGenericDAO;
-import ru.runa.wfe.definition.dao.IProcessDefinitionLoader;
+import ru.runa.wfe.audit.dao.ProcessLogDao;
+import ru.runa.wfe.definition.dao.ProcessDefinitionLoader;
 import ru.runa.wfe.execution.ExecutionContext;
-import ru.runa.wfe.execution.IExecutionContextFactory;
+import ru.runa.wfe.execution.ExecutionContextFactory;
 import ru.runa.wfe.presentation.BatchPresentation;
-import ru.runa.wfe.presentation.hibernate.IBatchPresentationCompilerFactory;
+import ru.runa.wfe.presentation.hibernate.BatchPresentationCompilerFactory;
 import ru.runa.wfe.ss.SubstitutionCriteria;
-import ru.runa.wfe.ss.logic.ISubstitutionLogic;
+import ru.runa.wfe.ss.logic.SubstitutionLogic;
 import ru.runa.wfe.task.Task;
-import ru.runa.wfe.task.dto.IWfTaskFactory;
+import ru.runa.wfe.task.dao.TaskDao;
+import ru.runa.wfe.task.dto.WfTaskFactory;
 import ru.runa.wfe.user.Actor;
 import ru.runa.wfe.user.EscalationGroup;
 import ru.runa.wfe.user.Executor;
-import ru.runa.wfe.user.dao.IExecutorDAO;
+import ru.runa.wfe.user.dao.ExecutorDao;
 
 public abstract class TestCaseDataSet implements ITestCaseDataSet {
 
     @Override
-    public void mockRules(IWfTaskFactory tastFactory) {
+    public void mockRules(WfTaskFactory tastFactory) {
 
     }
 
     @Override
-    public void mockRules(IExecutorDAO executorDAO) {
+    public void mockRules(ExecutorDao executorDao) {
 
     }
 
     @Override
-    public void mockRules(ISubstitutionLogic substitutionLogic) {
+    public void mockRules(SubstitutionLogic substitutionLogic) {
 
     }
 
     @Override
-    public void mockRules(IProcessDefinitionLoader processDefinitionLoader) {
+    public void mockRules(ProcessDefinitionLoader processDefinitionLoader) {
 
     }
 
     @Override
-    public void mockRules(IGenericDAO<Task> taskDAO) {
+    public void mockRules(TaskDao taskDao) {
 
     }
 
     @Override
-    public void mockRules(IExecutionContextFactory exeContextFactory) {
+    public void mockRules(ExecutionContextFactory executionContextFactory) {
 
     }
 
     @Override
-    public void mockRules(IBatchPresentationCompilerFactory<?> batchCompilerFactory) {
+    public void mockRules(BatchPresentationCompilerFactory<?> batchCompilerFactory) {
 
     }
 
     @Override
-    public void mockRules(IProcessLogDAO<ProcessLog> logDAO) {
+    public void mockRules(ProcessLogDao logDAO) {
 
     }
 

wfe-service/src/main/java/ru/runa/wfe/service/ExecutionService.java
@@ -17,9 +17,10 @@
  */
 package ru.runa.wfe.service;
 
+import ru.runa.wfe.var.file.FileVariable;
+
 import java.util.List;
 import java.util.Map;
-
 import ru.runa.wfe.audit.ProcessLogFilter;
 import ru.runa.wfe.definition.DefinitionDoesNotExistException;
 import ru.runa.wfe.execution.ParentProcessExistsException;
@@ -36,7 +37,6 @@ import ru.runa.wfe.user.User;
 import ru.runa.wfe.validation.ValidationException;
 import ru.runa.wfe.var.dto.WfVariable;
 import ru.runa.wfe.var.dto.WfVariableHistoryState;
-import ru.runa.wfe.var.file.FileVariable;
 
 /**
  * Process execution service.
@@ -262,7 +262,7 @@ public interface ExecutionService {
      *            process id
      * @param variableName
      *            variable name
-     * @return IFileVariable or <code>null</code>
+     * @return FileVariable or <code>null</code>
      * @throws ProcessDoesNotExistException
      */
     public FileVariable getFileVariableValue(User user, Long processId, String variableName) throws ProcessDoesNotExistException;

wfe-service/src/main/java/ru/runa/wfe/service/delegate/ExecutionServiceDelegate.java
@@ -17,9 +17,10 @@
  */
 package ru.runa.wfe.service.delegate;
 
+import ru.runa.wfe.var.file.FileVariable;
+
 import java.util.List;
 import java.util.Map;
-
 import ru.runa.wfe.audit.ProcessLogFilter;
 import ru.runa.wfe.execution.ProcessDoesNotExistException;
 import ru.runa.wfe.execution.ProcessFilter;
@@ -34,12 +35,11 @@ import ru.runa.wfe.user.Executor;
 import ru.runa.wfe.user.User;
 import ru.runa.wfe.var.dto.WfVariable;
 import ru.runa.wfe.var.dto.WfVariableHistoryState;
-import ru.runa.wfe.var.file.FileVariable;
 
 /**
  * Created on 28.09.2004
  */
-public class ExecutionServiceDelegate extends EJB3Delegate implements ExecutionService {
+public class ExecutionServiceDelegate extends Ejb3Delegate implements ExecutionService {
 
     public ExecutionServiceDelegate() {
         super(ExecutionService.class);