4de0577622c7b876133218e3026a88972f308563
Alexander Usatov <lex.9yards@gmail.com>
2018-11-01 17:26:22 +0300

    RM 800 refactoring

--- wfe-core/src/main/java/ru/runa/wfe/extension/handler/ExecutorsListFromGroupHandler.java
+++ wfe-core/src/main/java/ru/runa/wfe/extension/handler/ExecutorsListFromGroupHandler.java
@@ -1,5 +1,6 @@
 package ru.runa.wfe.extension.handler;
 
+import com.google.common.collect.Lists;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.runa.wfe.user.Executor;
@@ -12,8 +13,13 @@ public class ExecutorsListFromGroupHandler extends CommonParamBasedHandler {
     
     @Override
     protected void executeAction(HandlerData handlerData) throws Exception {
-        Group group = handlerData.getInputParamValueNotNull(Group.class, "group");
-        List<Executor> executors = executorDao.getAllNonGroupExecutorsFromGroup(group);
+        String groupName = handlerData.getInputParamValueNotNull(String.class, "group");
+        List<Executor> executors;
+        try {
+            executors = executorDao.getAllNonGroupExecutorsFromGroup(executorDao.getGroup(groupName));
+        } catch(Exception e) {
+            executors = Lists.newArrayList();
+        }
         handlerData.setOutputParam("executors", executors);
     }
 
