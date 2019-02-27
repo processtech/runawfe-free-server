e6f88fc11b29976e6d09e880a717364b62159772
dataobject <dataobject@yandex.ru>
2019-02-27 16:01:57 +0300

    RM973

--- wfe-web/src/main/java/ru/runa/wf/web/action/SubmitStartProcessFormAction.java
+++ wfe-web/src/main/java/ru/runa/wf/web/action/SubmitStartProcessFormAction.java
@@ -18,14 +18,11 @@
 package ru.runa.wf.web.action;
 
 import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
-
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
-
 import ru.runa.common.web.Commons;
 import ru.runa.common.web.Resources;
 import ru.runa.common.web.form.IdForm;
@@ -59,6 +56,9 @@ public class SubmitStartProcessFormAction extends BaseProcessFormAction {
         Map<String, Object> variables = getFormVariables(request, actionForm, interaction, new DelegateDefinitionVariableProvider(user, definitionId));
         String transitionName = ((CommonProcessForm) actionForm).getSubmitButton();
         variables.put(WfProcess.SELECTED_TRANSITION_KEY, transitionName);
+        String startSwimlaneName = Delegates.getDefinitionService().getParsedProcessDefinition(user, definitionId).getStartStateNotNull()
+                .getFirstTaskNotNull().getSwimlane().getName();
+        variables.put(startSwimlaneName, profile.getActor());
         WfDefinition definition = Delegates.getDefinitionService().getProcessDefinition(user, definitionId);
         log.debug(user + " submitted start form for definition " + definition.getName());
         Long processId = Delegates.getExecutionService().startProcess(user, definition.getName(), variables);
