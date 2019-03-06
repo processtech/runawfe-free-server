ef64dc5f7d3a8ec888c3ce0e2ddd900210e6a2f4
andlap992 <andlap992@gmail.com>
2019-03-06 23:56:28 +0300

    rm929 add signal check

--- wfe-core/src/main/java/ru/runa/wfe/lang/SendMessageNode.java
+++ wfe-core/src/main/java/ru/runa/wfe/lang/SendMessageNode.java
@@ -17,20 +17,34 @@
  */
 package ru.runa.wfe.lang;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.jms.ObjectMessage;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import ru.runa.wfe.audit.SendMessageLog;
+import ru.runa.wfe.commons.TypeConversionUtil;
 import ru.runa.wfe.commons.Utils;
 import ru.runa.wfe.commons.ftl.ExpressionEvaluator;
 import ru.runa.wfe.execution.ExecutionContext;
+import ru.runa.wfe.execution.Token;
+import ru.runa.wfe.execution.logic.ExecutionLogic;
+import ru.runa.wfe.lang.bpmn2.CatchEventNode;
 import ru.runa.wfe.var.MapDelegableVariableProvider;
+import ru.runa.wfe.var.VariableMapping;
 import ru.runa.wfe.var.dto.Variables;
 
 import com.google.common.collect.Maps;
 
 public class SendMessageNode extends BaseMessageNode {
+    @Autowired
+    private CatchEventNode catchEventNode;
+
+    @Autowired
+    private ExecutionLogic executionLogic;
+
     private static final long serialVersionUID = 1L;
 
     private String ttlDuration;
@@ -58,6 +72,22 @@ public class SendMessageNode extends BaseMessageNode {
         variables.put(Variables.CURRENT_NODE_NAME, executionContext.getNode().getName());
         variables.put(Variables.CURRENT_NODE_ID, executionContext.getNode().getNodeId());
         MapDelegableVariableProvider variableProvider = new MapDelegableVariableProvider(variables, executionContext.getVariableProvider());
+
+        if (catchEventNode.getBoundaryEvents().size() == 1) {
+            HashMap<String, String> routingData = new HashMap<>();
+            for (VariableMapping variableMapping : getVariableMappings()) {
+                if (variableMapping.isPropertySelector()) {
+                    Object value = ExpressionEvaluator.evaluateVariableNotNull(variableProvider, variableMapping.getMappedName());
+                    String stringValue = TypeConversionUtil.convertTo(String.class, value);
+                    routingData.put(variableMapping.getName(), stringValue);
+                    List<Token> tokens = executionLogic.findTokensForMessageSelector(routingData);
+                    if (tokens.isEmpty()) {
+                        throw new Exception();
+                    }
+                }
+            }
+        }
+
         long ttl = ExpressionEvaluator.evaluateDuration(executionContext.getVariableProvider(), ttlDuration);
         ObjectMessage message = Utils.sendBpmnMessage(variableMappings, variableProvider, ttl);
         String log = Utils.toString(message, true);
