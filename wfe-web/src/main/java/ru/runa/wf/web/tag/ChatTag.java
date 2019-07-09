415a3dd095c599336ed4f399d96128af9dac2c3a
AlexandrBlueRose <mr.snow3@yandex.ru>
2019-07-09 23:28:53 +0300

    Исправлено множество ошибок визуала

--- wfe-web/src/main/java/ru/runa/wf/web/tag/ChatTag.java
+++ wfe-web/src/main/java/ru/runa/wf/web/tag/ChatTag.java
@@ -2,12 +2,9 @@ package ru.runa.wf.web.tag;
 
 import org.apache.ecs.html.TD;
 import org.apache.ecs.html.TR;
-import org.apache.ecs.html.Table;
 import org.apache.ecs.html.Button;
 import org.apache.ecs.html.Input;
-import org.apache.ecs.html.Link;
 import org.apache.ecs.html.Script;
-import org.apache.ecs.html.Span;
 import org.apache.ecs.html.Style;
 import org.apache.ecs.html.TextArea;
 import org.apache.ecs.html.Div;
@@ -17,9 +14,10 @@ import org.tldgen.annotations.Attribute;
 import org.tldgen.annotations.BodyContent;
 
 import ru.runa.wfe.security.Permission;
+import ru.runa.wfe.service.delegate.Delegates;
 
 @org.tldgen.annotations.Tag(bodyContent = BodyContent.JSP, name = "ChatTag")
-public class ChatTag extends ProcessBaseFormTag {//ToDo replace extends class for chat
+public class ChatTag extends ProcessBaseFormTag {
 
 private static final long serialVersionUID = -4722799699002222875L;
 
@@ -35,7 +33,7 @@ return test;
 
 @Override
 protected void fillFormData(TD tdFormElement) {
-// TODO Auto-generated method stub
+
 IMG imageOpenChat=new IMG();
 imageOpenChat.setSrc("/wfe/images/chat_expand.png");
 imageOpenChat.setID("imgButton");
@@ -60,16 +58,14 @@ buttonOpenChat.addElement("Открыть чат");
 
 
 Button upScaleChat=new Button();
-upScaleChat.addElement(imageOpenChat);
+upScaleChat.addElement(imageCloseChat);
 upScaleChat.setID("btnOp");
 upScaleChat.setType("button");
-//upScaleChat.setStyle("");
 
 Button sendMessageChat=new Button();
 sendMessageChat.addElement("Отправить");
 sendMessageChat.setID("btnSend");
 sendMessageChat.setType("button");
-//sendMessageChat.setOnClick("send()");
 
 Button closeChat=new Button();
 closeChat.setID("close");
@@ -79,8 +75,6 @@ closeChat.setType("button");
 TextArea messageSend=new TextArea();
 messageSend.setName("message");
 messageSend.setID("message");
-messageSend.setStyle("font-family: Verdana, Arial, Helvetica, sans-serif;\r\n" + 
-		"            font-size: 12pt; /* Размер шрифта в пунктах */");
 
 Input sendToAdress=new Input();
 sendToAdress.setType("text");
@@ -99,13 +93,20 @@ Div modalHeader=new Div();
 Div modalHeaderDragg=new Div();
 modalHeaderDragg.setClass("modal-header-dragg");
 modalHeaderDragg.setID("modal-header-dragg");
-modalHeaderDragg.setStyle("cursor: move;width: 220px;height:10px");
+
+
 modalHeader.setClass("modal-header");
 modalHeader.setID("modal-header");
 //modalHeader.setStyle("cursor: move");
+Button loadNewMessage=new Button();
+loadNewMessage.setID("loadNewBessageButton");
+loadNewMessage.addElement("Загрузить сообщения выше");
+loadNewMessage.setType("button");
 
 Div modalBody=new Div();
 modalBody.setClass("modal-body");
+if(Delegates.getExecutorService().isAdministrator(getUser()))
+    modalBody.addAttribute("admin", "true");//!
 modalBody.setStyle("height: 396px; width: 304px;");
 
 Div modalFooter=new Div();
@@ -113,24 +114,16 @@ modalFooter.setClass("modal-footer");
 
 Style styleButton=new Style();
 
-
-
-
-
-
-
-
-styleButton.addElement("#myBtn{width: 100px;float:right;} #btnOp{background-color:#e9eaed ;} #btnCl{background-color:#e9eaed ;}  .modal {display: none;position: fixed; padding-top: 480px; left: 0;top: 0; padding-left:20%; width: 100vw;height: 100vh; overflow: auto;}.modal-content {position: relative;background-color: #e9eaed;margin: auto;padding: 0; border: 5px solid #e9eaed;width: 30vw;height: 40vh;box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19); -webkit-animation-name: animatetop;-webkit-animation-duration: 0.4s;animation-name: animatetop;animation-duration: 0.4s} .close {color: white; float: right;font-size: 28px;font-weight: bold;} .modal-header{padding: 2px 16px;background-color: #e9eaed;color: white; height: 30px;font-size: 11pt;} .modal-body {padding: 2px 16px;background-color: #e1e2e5;font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 11pt;height: 390px;overflow: scroll;border: 1px solid #333;} .modal-footer {padding: 2px 16px; background-color: #e9eaed;color: white;} textarea { resize: none;}");
+styleButton.addElement("#myBtn{width: 100px;float:right;} #btnOp{background-color:#e9eaed ;} #btnCl{background-color:#e9eaed ;}  .modal {display: none;position: fixed; padding-top: 480px; left: 0;top: 0; padding-left:20%; width: 100vw;height: 100vh; overflow: auto;}.modal-content {position: relative;background-color: #e9eaed;margin: auto;padding: 0; border: 5px solid #e9eaed;width: 30vw;height: 40vh;box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2),0 6px 20px 0 rgba(0,0,0,0.19); -webkit-animation-name: animatetop;-webkit-animation-duration: 0.4s;animation-name: animatetop;animation-duration: 0.4s} .close {color: white; float: right;font-size: 28px;font-weight: bold;} .modal-header{padding: 2px 16px;background-color: #e9eaed;color: white; height: 30px;font-size: 11pt;} .modal-body {padding: 2px 16px;background-color: #e1e2e5;font-family: Verdana, Arial, Helvetica, sans-serif;font-size: 11pt;height: 390px;overflow: scroll;border: 1px solid #333;} .modal-footer {padding: 2px 16px; background-color: #e9eaed;color: white;} ");
 modalHeader.addElement(modalHeaderDragg);
 modalHeader.addElement(closeChat);
 modalHeader.addElement(upScaleChat);
 
 modalContetnt.addElement(modalHeader);
-
+modalBody.addElement(loadNewMessage);
 modalContetnt.addElement(modalBody);
 
 modalFooter.addElement(messageSend);
-//modalFooter.addElement(sendToAdress);
 modalFooter.addElement(sendMessageChat);
 modalContetnt.addElement(modalFooter);
 Script scrFromFile=new Script();
@@ -149,7 +142,6 @@ tdFormElement.addElement(box);
 
 @Override
 protected Permission getSubmitPermission() {
-// TODO Auto-generated method stub
 return Permission.LIST;
 }
 
--- wfe-web/src/main/webapp/js/chatPart1.js
+++ wfe-web/src/main/webapp/js/chatPart1.js
@@ -1,30 +1,82 @@
 $(document).ready(function() {
-	var nameAuthor=authorDataCheak()
 	var attachedPosts=[];
+	//флаг развернутого чата (0 - свернут, 1 - развернут)
 	var switchCheak=0;
 	var chatForm=document.getElementById('ChatForm');
-	var btn = document.getElementById("openChatButton");
+	//кнопка открытия чата
+	var btnOpenChat = document.getElementById("openChatButton");
+	
+	var btnLoadNewMessage=document.getElementById("loadNewBessageButton");
 	var btn2 = document.getElementById("btnSend");
 	var btnOp=document.getElementById("btnOp");
 	var imgButton=document.getElementById("imgButton");
-	var flag=1;
+	//флаг обозначающий состояние(развернут или свернут)ли чат
+	let flagRollExpandChat=0;
+	//закрытие (сворачивание) чата
 	var span = document.getElementById("close");
-	var lastMessageId=0;
+	//нумерация сообщений = количество загруженных сообщений, если небыло удалений
+	var lastMessageIndex=0;
+	var minMassageId = -1;
+	var maxMassageId = -1;
+	var currentMessageId = -1;
+	var numberNewMessages = 0;
+	
+	var blocOldMes=0;
 	//тест сокет
-	var chatSocket = new WebSocket("ws://localhost:8080/wfe/actions");
+	var chatSocketURL = "ws://" + document.location.host + "/wfe/chatSoket?chatId="+$('#ChatForm').attr('chatId');
+	var chatSocket = new WebSocket(chatSocketURL);
 	chatSocket.onmessage = onMessage;
+	function newxtMessages(count0){
+		   let newMessage={};
+		   newMessage.chatId=$('#ChatForm').attr('chatId');
+		   newMessage.type="getMessages";
+		   newMessage.lastMessageId=minMassageId;
+		   newMessage.Count = count0; //количество начальных сообщений
+		   let firstMessages = JSON.stringify(newMessage);
+		   chatSocket.send(firstMessages);
+	}
+	//функция пишущая кол-во непрочитанных сообщений = numberNewMessages
+	function updatenumberNewMessages(numberNewMessages0){ //доделать в конец функции вывод numberNewMessages!
+		numberNewMessages = numberNewMessages0;
+	}
+	chatSocket.onopen=function(){
+		//запрос 20 последних сообщений
+		newxtMessages(20);
+	    //запрос текущей информации по юзеру
+		   let newMessage2={};
+		   newMessage2.chatId=$('#ChatForm').attr('chatId');
+		   newMessage2.type="getChatUserInfo";
+		   let sendObject0 = JSON.stringify(newMessage2);
+		   chatSocket.send(sendObject0);
+	}
 	chatSocket.onclose = function(){
 		$(".modal-body").append("<table ><td>" + "потерянно соединение с чатом сервера"+ "</td></table >");
 	}
 //-----------
-	btn.onclick = function() {
+	btnLoadNewMessage.onclick=function(){
+		if(blocOldMes==0){
+		   blocOldMes=1;
+		//запрос 20 сообщений старых
+		   newxtMessages(20);
+		}
+	}
+	//кнопка открытия чата
+	btnOpenChat.onclick = function() {
 		if(chatForm!=null){
 			chatForm.style.display = "block";
 			switchCheak=1;
-		
+			//"в прочитанные все"
+			currentMessageId = maxMassageId;
+			numberNewMessages=0;
+			let newSend0={};
+			newSend0.chatId=$('#ChatForm').attr('chatId');
+			newSend0.type="setChatUserInfo";
+			newSend0.currentMessageId=currentMessageId;
+			let sendObject0 = JSON.stringify(newSend0);
+			chatSocket.send(sendObject0);
 		}
 	}
-
+	//закрытие (сворачивание) чата
 	span.onclick = function() {
 		chatForm.style.display = "none";
 		switchCheak=0;
@@ -34,29 +86,44 @@ $(document).ready(function() {
    var inputH=document.getElementById("message");
    var heightModalC=$('.modal-content').height();
    var widthModalC=$('.modal-content').width();
-   inputH.style.width = "250px";
-   inputH.style.height = "25px";
-   btnSend.onclick=function send() { 
+   
+   //
+	$('textarea').keypress(function(e) {
+	    if (e.which == 13){
+	    var text = $(this).val();
+		$(this).val(text + '\n ');
+	    return false;
+	    }
+	});
+   btnSend.onclick=function send() {
    let message = document.getElementById("message").value;
+   message = message.replace(/\n/g, "<br/>");
    let idHierarchyMessage="";
+
    for(var i=0;i<attachedPosts.length;i++){
 	   idHierarchyMessage+=attachedPosts[i]+":";
    }
-   
    //сокет
    let newMessage={};
    newMessage.message=message;
    newMessage.chatId=$('#ChatForm').attr('chatId');
    newMessage.idHierarchyMessage = idHierarchyMessage;
    newMessage.type="newMessage";
-   newMessage.author=nameAuthor.responseText;
    chatSocket.send(JSON.stringify(newMessage));
+   //чистим "ответы"
+   let addReplys0 = document.getElementsByClassName("addReply");
+   for(let i=0; i<addReplys0.length; i++){
+		$(addReplys0[i]).text("Ответить");
+		$(addReplys0[i]).attr("flagAttach", "false");
+   }
+   //
    attachedPosts=[];
    }
   
+   //кнопка развернуть/свернуть чат
    btnOp.onclick=function(){
-	   if(flag==1){
-		flag=0;
+	   if(flagRollExpandChat==0){
+		flagRollExpandChat=1;
 	   	$('.modal-content').css({
 	   		width: widthModalC+300,
 	   	    height: heightModalC+300,
@@ -65,6 +132,7 @@ $(document).ready(function() {
 	   	$('.modal-body').css({
 	   		height:'630px',
 	   	    width: '590px',
+	   	     
 	   	});
 		$('.modal-header').css({
 	   		
@@ -74,21 +142,23 @@ $(document).ready(function() {
 	   		
 	   	    width: '530px',
 	   	});
-		
-		inputH.style.width = "600px";
-		inputH.style.height = "70px";
-		imgButton.src="/wfe/images/chat_roll_up.png";
-	   }else if(flag==0){
-		   imgButton.src="/wfe/images/chat_expand.png";
-		   flag=1;
+		$('.modal-footer').css({
+	   		
+	   	    height: '80px',
+	   	});
+		imgButton.src="/wfe/images/chat_expand.png";
+	   }else if(flagRollExpandChat==1){
+		   
+		   flagRollExpandChat=0;
 		   $('.modal-content').css({
 				 width: '346px',
 				 height: '506px',
 		   	});
+		  
 			$('.modal-body').css({
 				width: '304px',
 		   	    height: '396px',
-		   	});
+		   	});	
 			$('.modal-header').css({
 		   		
 		   	    width: '316px',
@@ -97,12 +167,13 @@ $(document).ready(function() {
 		   		
 		   	    width: '220px',
 		   	});
-			inputH.style.width = "250px";
-			inputH.style.height = "25px";
-			//$('#btnOp').show();
-		   	//$('#btnCl').hide();
+			$('.modal-footer').css({
+		   		
+		   	    height: '53px',
+		   	});
+			imgButton.src="/wfe/images/chat_roll_up.png";
 	   }
-	   	}
+   }
    
    function hierarhyCheak(messageId){
 	 let urlString = "/wfe/ajaxcmd?command=GetHierarhyLevel&chatId="+$('#ChatForm').attr('chatId')+"&messageId="+messageId;	
@@ -121,7 +192,7 @@ $(document).ready(function() {
 							let hierarhyMass="";
 							//тут получаем id вложенных
 							if(data.messages[mes].hierarchyMessageFlag==1){
-								hierarhyMass+="<tr><td><a class=\"openHierarchy\" mesNumber=\""+data.messages[mes].id+"\" loadFlag=\"0\" openFlag=\"0\">Развернуть</a><div class=\"loadedHierarchy\"></div></td></tr>";
+								hierarhyMass+="<tr><td><a class=\"openHierarchy\" mesId=\""+data.messages[mes].id+"\" loadFlag=\"0\" openFlag=\"0\">Развернуть</a><div class=\"loadedHierarchy\"></div></td></tr>";
 							}
 							messageBody+="</td></tr>" + hierarhyMass;
 							messageBody+= "</table >";
@@ -135,22 +206,6 @@ $(document).ready(function() {
 			}
 	 });
    }
-   
-   function authorDataCheak(){
-		 let urlString = "/wfe/ajaxcmd?command=GetAuthorData&chatId="+$('#ChatForm').attr('chatId');
-		 return $.ajax({
-				type: "POST",
-				url: urlString,
-				dataType: "json",
-				contentType: "application/json; charset=UTF-8",
-				processData: false,
-				success: function(data) {
-					let ajaxRet="";
-					ajaxRet=data.author;
-						return ajaxRet;
-				}//if(data.newMessage==0) конец
-		 });
-	   }
    
    function addOnClickHierarchyOpen(){
 	   let elements = $(".openHierarchy");
@@ -172,8 +227,7 @@ $(document).ready(function() {
 					}else{
 						let thisElem=$(".openHierarchy")[0];
 						let element=this;
-						//$(this).next(".loadedHierarchy")[0].append(hierarhyCheak($(this).attr("mesNumber")));
-						hierarhyCheak($(element).attr("mesNumber")).then(ajaxRet=>{
+						hierarhyCheak($(element).attr("mesId")).then(ajaxRet=>{
 							$(this).next(".loadedHierarchy").append(getMessagesText(ajaxRet));						
 							addOnClickHierarchyOpen();
 							$(element).attr("loadFlag", "1");
@@ -184,7 +238,6 @@ $(document).ready(function() {
 					}
 				}
 			});
-	   //}
    }
 	function getMessagesText(data) {
 		let ajaxRet="";
@@ -195,7 +248,7 @@ $(document).ready(function() {
 					let hierarhyMass="";
 					//тут получаем id вложенных
 					if(data.messages[mes].hierarchyMessageFlag==1){
-						hierarhyMass+="<tr><td><a class=\"openHierarchy\" mesNumber=\""+data.messages[mes].id+"\" loadFlag=\"0\" openFlag=\"0\">Развернуть</a><div class=\"loadedHierarchy\"></div></td></tr>";
+						hierarhyMass+="<tr><td><a class=\"openHierarchy\" mesId=\""+data.messages[mes].id+"\" loadFlag=\"0\" openFlag=\"0\">Развернуть</a><div class=\"loadedHierarchy\"></div></td></tr>";
 					}
 					messageBody+="</td></tr>" + hierarhyMass;
 					messageBody+= "</table >";
@@ -209,53 +262,102 @@ $(document).ready(function() {
 	
 	//функция приёма сообщения
 	function addMessages(data){
-		var flagAttach=0;
 		if(data.newMessage==0){
 			for(let mes=0;mes<data.messages.length;mes++){
 				if(data.messages[mes].text !=null){
-					var messageBody="<table class=\"selectionTextQuote\"><tr><td>"+data.messages[mes].author+":"+data.messages[mes].text ;
+					if((minMassageId>data.messages[mes].id) || (minMassageId==-1)){
+						minMassageId=data.messages[mes].id;
+					}
+					
+					if((maxMassageId<data.messages[mes].id)){
+						maxMassageId=data.messages[mes].id;
+					}
+					
+					let text0 = data.messages[mes].text;
+					text0.replace(/\n/ig,"<br/>");
+					var messageBody="<table class=\"selectionTextQuote\"><tr><td><div class=\"author\" class=\"author\">"+data.messages[mes].author+"</div> :"+text0 ;
 					var hierarhyMass="";
 					//тут получаем id вложенных
 					if(data.messages[mes].hierarchyMessageFlag==1){
-						hierarhyMass+="<tr><td><a class=\"openHierarchy\" mesNumber=\""+data.messages[mes].id+"\" loadFlag=\"0\" openFlag=\"0\">Развернуть</a><div class=\"loadedHierarchy\"></div></td></tr>";
+						hierarhyMass+="<tr><td><a class=\"openHierarchy\" mesId=\""+data.messages[mes].id+"\" loadFlag=\"0\" openFlag=\"0\">Развернуть</a><div class=\"loadedHierarchy\"></div></td></tr>";
 					}
+					//"развернуть"
 					messageBody+="</td></tr>" + hierarhyMass;
-					messageBody+= "<tr><td>"+ data.messages[mes].dateTime + "</td><td><a class=\"addReply\" id=\"messReply"+(lastMessageId)+"\" mesNumber=\""+data.messages[mes].id+"\"> Ответить</a></td></tr></table >";
-					$(".modal-body").append(messageBody);
-					if(flagAttach==0){
-					document.getElementById("messReply"+(lastMessageId+mes)).onclick=function(){
-						attachedPosts.push($(this).attr("mesNumber"));
-						$(this).text("Ответить");
-						flagAttach=1;}
+					//"ответить"
+					messageBody+= "<tr><td><hr class=\"hr-dashed\">"+ data.messages[mes].dateTime + "</td><td><div class=\"hr-dashed-vertical\"><a class=\"addReply\" id=\"messReply"+(lastMessageIndex)+"\" mesId=\""+data.messages[mes].id+"\" flagAttach=\"false\"> Ответить</a></div></td></tr>";
+					//админ
+					if($(".modal-body").attr("admin")=="true"){
+						messageBody+="<tr><td>"+"<hr class=\"hr-dashed\"><a class=\"deleterMessage\" id=\"messDeleter"+(lastMessageIndex)+"\" mesId=\""+data.messages[mes].id+"\">удалить</a>"+"</td></tr>";
 					}
-					else if(flagAttach==1){
-						document.getElementById("messReply"+(lastMessageId+mes)).onclick=function(){
-							attachedPosts.push($(this).attr("mesNumber"));
-							$(this).text("Отменить");
-							flagAttach=0;
+					//конец
+					messageBody+="</table >";
+					
+					if(data.old == 0){
+						$(".modal-body").append(messageBody);
+						if(switchCheak==0){//+1 непрочитанное сообщение
+							updatenumberNewMessages(numberNewMessages+1);
 						}
 					}
+					else{
+						$(".modal-body").children().first().after(messageBody);
+					}
+					
+					document.getElementById("messReply"+(lastMessageIndex/*+mes*/)).onclick=function(){
+						if($(this).attr("flagAttach")=="false"){
+							attachedPosts.push($(this).attr("mesId"));
+							$(this).attr("flagAttach", "true");
+							$(this).text("Отменить");
+						}
+						else{
+							$(this).text("Ответить");
+							$(this).attr("flagAttach", "false");
+							let pos0 = attachedPosts.indexOf($(this).attr("mesId"), 0);
+							attachedPosts.splice(pos0, 1);
+						}
+					}
+					document.getElementById("messDeleter"+(lastMessageIndex/*+mes*/)).onclick=deleteMessage;
 					addOnClickHierarchyOpen();
-					lastMessageId+=1;
+					lastMessageIndex+=1;
 				}
 			}
 		}//if(data.newMessage==0) конец
 	}
-	//бновление свернутого чата, пока не работает (30,05,2019)
+	//бновление свернутого чата, пока не работает (30,05,20019)
 	function addNewMessagesCount(data){
 		if(data.newMessageCount>0){
 			 document.getElementById("indicateNewMessage").append("<td>ok</td>");
 		}
 	}
+	//удаление сообщений
+	function deleteMessage(){
+		   //сокет
+		   let newMessage={};
+		   newMessage.messageId=$(this).attr("mesId");
+		   newMessage.chatId=$('#ChatForm').attr('chatId');
+		   newMessage.type="deleteMessage";
+		   chatSocket.send(JSON.stringify(newMessage));
+		   $(this).parent().parent().parent().parent().remove();
+	}
 	//тест сокет
 	//приём с сервера
 	function onMessage(event) {
-		
 		let messsage0 = JSON.parse(event.data);
 		if(messsage0.messType=="newMessages"){
 			addMessages(messsage0);
 		}
+		else if(messsage0.messType=="deblocOldMes"){
+			blocOldMes=0;
+		}
+		else if(messsage0.messType=="ChatUserInfo"){
+			if(switchCheak == 0){
+				if(currentMessageId<messsage0.lastMessageId)
+					currentMessageId = messsage0.lastMessageId;
+				updatenumberNewMessages(messsage0.numberNewMessages);
+				//дозапрос всех непрочитанных сообщений
+				if(numberNewMessages>lastMessageIndex)
+					newxtMessages(numberNewMessages-lastMessageIndex);
+			}
+		}
 	}
-	
 	//конец
    });
