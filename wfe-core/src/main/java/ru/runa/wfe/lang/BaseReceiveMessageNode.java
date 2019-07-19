d3f01f16cc3e4817360d47d854bc22b289968f6d
Dofs Serge <dofs197@gmail.com>
2019-07-19 18:53:45 +0300

    set swimlane value in boundary event only if reassign swimlane to task performer is set

--- wfe-core/src/main/java/ru/runa/wfe/lang/BaseReceiveMessageNode.java
+++ wfe-core/src/main/java/ru/runa/wfe/lang/BaseReceiveMessageNode.java
@@ -3,6 +3,7 @@ package ru.runa.wfe.lang;
 import com.google.common.collect.Lists;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import ru.runa.wfe.commons.Utils;
 import ru.runa.wfe.execution.ExecutionContext;
 import ru.runa.wfe.var.VariableMapping;
@@ -22,10 +23,21 @@ public class BaseReceiveMessageNode extends BaseMessageNode implements BoundaryE
     }
 
     public void leave(ExecutionContext executionContext, Map<String, Object> map) {
+        String swimlaneName = null;
+        boolean setSwimlaneValue = true;
+        if (this instanceof BoundaryEvent && getParentElement() instanceof BaseTaskNode) {
+            TaskDefinition taskDefinition = ((BaseTaskNode) getParentElement()).getFirstTaskNotNull();
+            swimlaneName = taskDefinition.getSwimlane().getName();
+            setSwimlaneValue = taskDefinition.isReassignSwimlaneToTaskPerformer();
+        }
         for (VariableMapping variableMapping : getVariableMappings()) {
             if (!variableMapping.isPropertySelector()) {
                 if (map.containsKey(variableMapping.getMappedName())) {
                     Object value = map.get(variableMapping.getMappedName());
+                    if (Objects.equals(swimlaneName, variableMapping.getName()) && !setSwimlaneValue) {
+                        log.warn("ignored to set swimlane value from signal for '" + variableMapping.getMappedName() + "'");
+                        continue;
+                    }
                     executionContext.setVariableValue(variableMapping.getName(), value);
                 } else {
                     log.warn("message does not contain value for '" + variableMapping.getMappedName() + "'");
