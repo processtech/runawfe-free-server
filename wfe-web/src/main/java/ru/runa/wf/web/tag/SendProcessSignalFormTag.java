3e218da5c5273ac628f5a7bc6232b2d545f0aef9
Куликов Денис Борисович <kulikovdb@sibedge.com>
2020-02-12 12:42:44 +0300

    rm1037 send process signal

--- wfe-web/src/main/java/ru/runa/wf/web/tag/SendProcessSignalFormTag.java
+++ wfe-web/src/main/java/ru/runa/wf/web/tag/SendProcessSignalFormTag.java
@@ -35,70 +35,7 @@ public class SendProcessSignalFormTag extends TitledFormTag {
     protected void fillFormData(TD tdFormElement) {
         addTable(tdFormElement, "routing", MessagesProcesses.ROUTING_PARAMETER_NAME, MessagesProcesses.ROUTING_PARAMETER_VALUE, "addRoutingRow()");
         addTable(tdFormElement, "payload", MessagesProcesses.PAYLOAD_PARAMETER_NAME, MessagesProcesses.PAYLOAD_PARAMETER_VALUE, "addPayloadRow()");
-        getRoutingJs(tdFormElement);
-        getPayloadJs(tdFormElement);
     }
-
-    private void getRoutingJs(TD tdFormElement) {
-        String js = "function addRoutingRow() {\n" +
-                "  var tableId = 'routing';\n" +
-                "  var table = document.getElementById('routing');\n" +
-                "  var len = table.getElementsByTagName(\"tr\").length - 1;\n" +
-                "  var index = len - 1;\n" +
-                "  var row = table.insertRow(len);\n" +
-                "  var cell1 = row.insertCell(0);\n" +
-                "  var cell2 = row.insertCell(1);\n" +
-                "  cell1.setAttribute(\"class\", \"list\");\n" +
-                "  cell2.setAttribute(\"class\", \"list\");\n" +
-                "  cell1.innerHTML = '<input type=\"TEXT\" name=\"' + tableId + 'Param(' + index + ')\"/>';\n" +
-                "  cell2.innerHTML = '<input type=\"TEXT\" name=\"' + tableId + 'Value(' + index + ')\"/>';\n" +
-                "}";
-
-        Script script = new Script();
-        script.setLanguage("javascript");
-        script.setType("text/javascript");
-        script.addElement(new StringElement(js));
-        tdFormElement.addElement(script);
-    }
-
-    private void getPayloadJs(TD tdFormElement) {
-        StringBuilder optionsBuilder = new StringBuilder();
-        for (String option : SignalUtils.getOptions()) {
-            optionsBuilder.append("<option value=\"")
-                    .append(option)
-                    .append("\"");
-            if (option.equals("string")) {
-                optionsBuilder.append(" selected");
-            }
-            optionsBuilder.append(">")
-                    .append(option)
-                    .append("</option>");
-
-        }
-        String js = "function addPayloadRow() {\n" +
-                "  var table = document.getElementById('payload');\n" +
-                "  var len = table.getElementsByTagName(\"tr\").length - 1;\n" +
-                "  var index = len - 1;\n" +
-                "  var row = table.insertRow(len);\n" +
-                "  var cell1 = row.insertCell(0);\n" +
-                "  var cell2 = row.insertCell(1);\n" +
-                "  cell1.setAttribute(\"class\", \"list\");\n" +
-                "  cell2.setAttribute(\"class\", \"list\");\n" +
-                "  cell1.innerHTML = '<input type=\"TEXT\" name=\"payloadParam(' + index + ')\"/>';\n" +
-                "  cell2.innerHTML = '<input type=\"TEXT\" style=\"width:200px\" name=\"payloadValue(' + index + ')\"/>" +
-                                     " <select name=\"payloadType(' + index + ')\">" +
-                                        optionsBuilder.toString() +
-                                     " </select> " +
-                "';\n" +
-                "}";
-
-        Script script = new Script();
-        script.setLanguage("javascript");
-        script.setType("text/javascript");
-        script.addElement(new StringElement(js));
-        tdFormElement.addElement(script);
-    }
-
 
     private void addTable(TD tdFormElement, String tableId, StrutsMessage paramName, StrutsMessage paramValue, String onclick) {
         Table table = new Table();
@@ -116,7 +53,6 @@ public class SendProcessSignalFormTag extends TitledFormTag {
         addTd.setOnClick(onclick);
         addTr.addElement(addTd);
         table.addElement(addTr);
-
     }
 
 
--- wfe-web/src/main/webapp/WEB-INF/wf/send_process_signal.jsp
+++ wfe-web/src/main/webapp/WEB-INF/wf/send_process_signal.jsp
@@ -3,6 +3,9 @@
 <%@ taglib uri="/WEB-INF/struts-html.tld" prefix="html" %>
 <%@ taglib uri="/WEB-INF/wf.tld" prefix="wf" %>
 <tiles:insert page="/WEB-INF/af/main_layout.jsp" flush="true">
+    <tiles:put name="head" type="string">
+		<script type="text/javascript" src="<html:rewrite page='<%="/js/sendprocesssingal.js"%>' />">c=0;</script>
+	</tiles:put>
     <tiles:put name="body" type="string">
         <wf:sendProcessSignalForm />
     </tiles:put>
