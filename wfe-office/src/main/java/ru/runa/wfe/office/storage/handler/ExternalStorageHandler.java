3325d98bf30a4de3cf8f0cca9ae92184b2564432
Mikhail Alekseev <malekseev@processtech.ru>
2020-08-12 15:07:48 +0300

    rm1766 #note-24: Удалена регистрация StoreOperation. Передача параметров скорректирована для разных хранилищ

--- wfe-office/src/main/java/ru/runa/wfe/office/storage/handler/ExternalStorageHandler.java
+++ wfe-office/src/main/java/ru/runa/wfe/office/storage/handler/ExternalStorageHandler.java
@@ -2,17 +2,16 @@ package ru.runa.wfe.office.storage.handler;
 
 import java.util.HashMap;
 import java.util.Map;
+import ru.runa.wfe.InternalApplicationException;
 import ru.runa.wfe.datasource.DataSourceStorage;
 import ru.runa.wfe.definition.FileDataProvider;
-import ru.runa.wfe.office.excel.OnSheetConstraints;
 import ru.runa.wfe.office.shared.FilesSupplierConfigParser;
 import ru.runa.wfe.office.shared.OfficeFilesSupplierHandler;
-import ru.runa.wfe.office.storage.StoreHelper;
 import ru.runa.wfe.office.storage.StoreService;
 import ru.runa.wfe.office.storage.binding.DataBinding;
 import ru.runa.wfe.office.storage.binding.DataBindings;
 import ru.runa.wfe.office.storage.binding.ExecutionResult;
-import ru.runa.wfe.office.storage.services.StoreHelperImpl;
+import ru.runa.wfe.office.storage.services.StoreHelper;
 import ru.runa.wfe.var.UserType;
 import ru.runa.wfe.var.VariableProvider;
 import ru.runa.wfe.var.dto.WfVariable;
@@ -32,35 +31,44 @@ public class ExternalStorageHandler extends OfficeFilesSupplierHandler<DataBindi
                 DataSourceStorage.parseDataSource(config.getInputFilePath(), variableProvider),
                 variableProvider
         );
-        final StoreHelper storeHelper = new StoreHelperImpl(config, variableProvider, storeService);
+        final StoreHelper storeHelper = new StoreHelper(config, variableProvider, storeService);
 
         for (DataBinding binding : config.getBindings()) {
-            final ExecutionResult executionResult = execute(variableProvider, binding, storeHelper);
-            if (executionResult.isNeedReturn()) {
-                result.put(binding.getVariableName(), executionResult.getValue());
+            try {
+                final ExecutionResult executionResult = execute(variableProvider, binding, storeHelper);
+                if (executionResult.isNeedReturn()) {
+                    result.put(binding.getVariableName(), executionResult.getValue());
+                }
+            } catch (Exception e) {
+                log.error("Error while executing operation with DataStore", e);
+                throw new InternalApplicationException(e);
             }
         }
         return result;
     }
 
-    protected ExecutionResult execute(VariableProvider variableProvider, DataBinding binding, StoreHelper storeHelper) {
+    protected ExecutionResult execute(VariableProvider variableProvider, DataBinding binding, StoreHelper storeHelper) throws Exception {
         binding.getConstraints().applyPlaceholders(variableProvider);
+        final WfVariable variable = variableProvider.getVariableNotNull(binding.getVariableName());
         switch (config.getQueryType()) {
-            case INSERT:
-            case UPDATE: {
-                final WfVariable variable = variableProvider.getVariableNotNull(binding.getVariableName());
+            case INSERT: {
                 storeHelper.setVariableFormat(variable.getDefinition().getFormatNotNull());
-                return storeHelper.execute(binding, variable);
+                return storeHelper.save(binding, variable);
+            }
+            case UPDATE: {
+                storeHelper.setVariableFormat(variable.getDefinition().getFormatNotNull());
+                return storeHelper.update(binding, variable, config.getCondition());
             }
             case SELECT: {
-                final WfVariable variable = variableProvider.getVariableNotNull(config.getOutputFileVariableName());
                 storeHelper.setVariableFormat(variable.getDefinition().getFormatNotNull());
-                return storeHelper.execute(binding, variableProvider.getUserType(((OnSheetConstraints) binding.getConstraints()).getSheetName()));
+                return storeHelper.findByFilter(binding, storeHelper.userType(variable), config.getCondition());
             }
-            default:
-                final UserType userType = variableProvider.getUserType(((OnSheetConstraints) binding.getConstraints()).getSheetName());
+            case DELETE:
+                final UserType userType = storeHelper.userType(variable);
                 storeHelper.setVariableFormat(new UserTypeFormat(userType));
-                return storeHelper.execute(binding, userType);
+                return storeHelper.delete(binding, userType, config.getCondition());
+            default:
+                throw new IllegalStateException("Unexpected value: " + config.getQueryType());
         }
     }
 }
--- wfe-office/src/main/java/ru/runa/wfe/office/storage/handler/InternalStorageHandler.java
+++ wfe-office/src/main/java/ru/runa/wfe/office/storage/handler/InternalStorageHandler.java
@@ -3,20 +3,32 @@ package ru.runa.wfe.office.storage.handler;
 import com.google.common.collect.Iterables;
 import java.util.HashMap;
 import java.util.Map;
+import ru.runa.wfe.InternalApplicationException;
 import ru.runa.wfe.datasource.DataSourceStorage;
 import ru.runa.wfe.definition.FileDataProvider;
-import ru.runa.wfe.office.storage.StoreHelper;
+import ru.runa.wfe.office.excel.OnSheetConstraints;
+import ru.runa.wfe.office.shared.FilesSupplierConfigParser;
+import ru.runa.wfe.office.shared.OfficeFilesSupplierHandler;
 import ru.runa.wfe.office.storage.StoreService;
 import ru.runa.wfe.office.storage.binding.DataBinding;
+import ru.runa.wfe.office.storage.binding.DataBindings;
 import ru.runa.wfe.office.storage.binding.ExecutionResult;
-import ru.runa.wfe.office.storage.services.StoreHelperImpl;
+import ru.runa.wfe.office.storage.services.StoreHelper;
+import ru.runa.wfe.var.UserType;
 import ru.runa.wfe.var.VariableProvider;
+import ru.runa.wfe.var.dto.WfVariable;
+import ru.runa.wfe.var.format.UserTypeFormat;
 
 /**
  * @author Alekseev Mikhail
  * @since #1507
  */
-public class InternalStorageHandler extends ExternalStorageHandler {
+public class InternalStorageHandler extends OfficeFilesSupplierHandler<DataBindings> {
+    @Override
+    protected FilesSupplierConfigParser<DataBindings> createParser() {
+        return new StorageBindingsParser();
+    }
+
     @Override
     protected Map<String, Object> executeAction(VariableProvider variableProvider, FileDataProvider fileDataProvider) {
         final Map<String, Object> result = new HashMap<>();
@@ -24,15 +36,51 @@ public class InternalStorageHandler extends ExternalStorageHandler {
                 DataSourceStorage.parseDataSource(config.getInputFilePath(), variableProvider),
                 variableProvider
         );
-        final StoreHelper storeHelper = new StoreHelperImpl(config, variableProvider, storeService);
+        final StoreHelper storeHelper = new StoreHelper(config, variableProvider, storeService);
 
         final DataBinding binding = Iterables.getOnlyElement(config.getBindings());
-        final ExecutionResult executionResult = execute(variableProvider, binding, storeHelper);
+        try {
+            final ExecutionResult executionResult = execute(variableProvider, binding, storeHelper);
 
-        if (executionResult.isNeedReturn()) {
-            result.put(config.getOutputFileVariableName() != null ?
-                    config.getOutputFileVariableName() : binding.getVariableName(), executionResult.getValue());
+            if (executionResult.isNeedReturn()) {
+                result.put(config.getOutputFileVariableName() != null ?
+                        config.getOutputFileVariableName() : binding.getVariableName(), executionResult.getValue());
+            }
+            return result;
+        } catch (Exception e) {
+            log.error("Error while executing operation with DataStore", e);
+            throw new InternalApplicationException(e);
         }
-        return result;
+    }
+
+    protected ExecutionResult execute(VariableProvider variableProvider, DataBinding binding, StoreHelper storeHelper) throws Exception {
+        binding.getConstraints().applyPlaceholders(variableProvider);
+        switch (config.getQueryType()) {
+            case INSERT: {
+                final WfVariable variable = variableProvider.getVariableNotNull(binding.getVariableName());
+                storeHelper.setVariableFormat(variable.getDefinition().getFormatNotNull());
+                return storeHelper.save(binding, variable);
+            }
+            case UPDATE: {
+                final WfVariable variable = variableProvider.getVariableNotNull(binding.getVariableName());
+                storeHelper.setVariableFormat(variable.getDefinition().getFormatNotNull());
+                return storeHelper.update(binding, variable, config.getCondition());
+            }
+            case SELECT: {
+                final WfVariable variable = variableProvider.getVariableNotNull(config.getOutputFileVariableName());
+                storeHelper.setVariableFormat(variable.getDefinition().getFormatNotNull());
+                return storeHelper.findByFilter(
+                        binding,
+                        variableProvider.getUserType(((OnSheetConstraints) binding.getConstraints()).getSheetName()),
+                        config.getCondition()
+                );
+            }
+            case DELETE:
+                final UserType userType = variableProvider.getUserType(((OnSheetConstraints) binding.getConstraints()).getSheetName());
+                storeHelper.setVariableFormat(new UserTypeFormat(userType));
+                return storeHelper.delete(binding, userType, config.getCondition());
+            default:
+                throw new IllegalStateException("Unexpected value: " + config.getQueryType());
+        }
     }
 }
