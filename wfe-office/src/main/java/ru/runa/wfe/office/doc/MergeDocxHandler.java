3ad936d3cf96a5efe1f6b73834b63e79edbb5d55
dataobject <dataobject@yandex.ru>
2020-09-09 23:29:26 +0300

    RM1071 https://rm.processtech.ru/issues/1071#note-24

--- wfe-office/src/main/java/ru/runa/wfe/office/doc/MergeDocxHandler.java
+++ wfe-office/src/main/java/ru/runa/wfe/office/doc/MergeDocxHandler.java
@@ -4,21 +4,25 @@ import com.google.common.collect.Maps;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.math.BigInteger;
 import java.util.List;
 import java.util.Map;
 import org.apache.poi.xwpf.converter.pdf.PdfConverter;
 import org.apache.poi.xwpf.converter.pdf.PdfOptions;
 import org.apache.poi.xwpf.usermodel.IBodyElement;
+import org.apache.poi.xwpf.usermodel.XWPFAbstractNum;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFHyperlinkRun;
+import org.apache.poi.xwpf.usermodel.XWPFNum;
+import org.apache.poi.xwpf.usermodel.XWPFNumbering;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFPicture;
-import org.apache.poi.xwpf.usermodel.XWPFRelation;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
 import org.apache.poi.xwpf.usermodel.XWPFStyle;
 import org.apache.poi.xwpf.usermodel.XWPFTable;
 import org.apache.xmlbeans.XmlObject;
-import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTHyperlink;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTAbstractNum;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTDecimalNumber;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTNumLvl;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTStyle;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.CTTbl;
 import ru.runa.wfe.definition.FileDataProvider;
@@ -43,6 +47,7 @@ public class MergeDocxHandler extends OfficeFilesSupplierHandler<MergeDocxConfig
             return result;
         }
         XWPFDocument document = null;
+        XWPFNumbering newNumbering = null;
         for (DocxInfo docxInfo : infos) {
             InputStream inputStream = config.getFileInputStream(variableProvider, fileDataProvider, docxInfo);
             XWPFDocument mergingDocument = new XWPFDocument(inputStream);
@@ -50,43 +55,49 @@ public class MergeDocxHandler extends OfficeFilesSupplierHandler<MergeDocxConfig
                 // first document used as base
                 document = mergingDocument;
             } else {
+                XWPFNum num = null;
+                XWPFNumbering mergingNumbering = mergingDocument.getNumbering();
+                if (mergingNumbering != null) {
+                    if (newNumbering == null) {
+                        newNumbering = document.createNumbering();
+                        newNumbering.addAbstractNum(
+                                new XWPFAbstractNum((CTAbstractNum) mergingNumbering.getAbstractNum(BigInteger.ZERO).getCTAbstractNum().copy()));
+                    }
+                    XWPFAbstractNum abstractNum = newNumbering.getAbstractNum(BigInteger.ZERO);
+                    BigInteger numId = newNumbering.addNum(abstractNum.getAbstractNum().getAbstractNumId());
+                    num = newNumbering.getNum(numId);
+                    CTNumLvl lvlOverride = num.getCTNum().addNewLvlOverride();
+                    lvlOverride.setIlvl(BigInteger.ZERO);
+                    CTDecimalNumber number = lvlOverride.addNewStartOverride();
+                    number.setVal(BigInteger.ONE);
+                }
                 for (IBodyElement bodyElement : mergingDocument.getBodyElements()) {
                     if (bodyElement instanceof XWPFParagraph) {
                         XWPFParagraph paragraph = (XWPFParagraph) bodyElement;
-                        List<XWPFRun> runs = paragraph.getRuns();
                         XWPFParagraph newParagraph = document.createParagraph();
-                        newParagraph.setStyle(paragraph.getStyle());
-                        newParagraph.setSpacingAfter(paragraph.getSpacingAfter());
-                        newParagraph.setSpacingBefore(paragraph.getSpacingBefore());
-                        newParagraph.setSpacingAfterLines(paragraph.getSpacingAfterLines());
-                        newParagraph.setSpacingBeforeLines(paragraph.getSpacingBeforeLines());
-                        newParagraph.setSpacingBetween(paragraph.getSpacingBetween());
-                        XWPFRun newRun = null;
-                        for (XWPFRun run : runs) {
-                            if (run instanceof XWPFHyperlinkRun) {
-                                XWPFHyperlinkRun hlRun = (XWPFHyperlinkRun) run;
-                                String rId = newParagraph
-                                        .getPart()
-                                        .getPackagePart()
-                                        .addExternalRelationship(hlRun.getCTHyperlink().getRArray(0).getTArray(0).getStringValue(),
-                                                XWPFRelation.HYPERLINK.getRelation()).getId();
-                                CTHyperlink newHyperlink = newParagraph.getCTP().addNewHyperlink();
-                                newHyperlink.set(hlRun.getCTHyperlink());
-                                newHyperlink.setId(rId);
-                                newRun = new XWPFHyperlinkRun(newHyperlink, newHyperlink.getRArray(0), newParagraph);
-                                newParagraph.addRun(newRun);
-                            } else {
-                                newRun = newParagraph.createRun();
+                        List<XWPFRun> runs = paragraph.getRuns();
+                        boolean picturesEmbedded = false;
+                        for (int i = 0; i < runs.size(); i++) {
+                            XWPFRun run = runs.get(i);
+                            if (run.getEmbeddedPictures().size() > 0) {
+                                XWPFRun newRun = newParagraph.createRun();
                                 newRun.getCTR().set(run.getCTR().copy());
+                                newRun.getEmbeddedPictures().clear();
+                                newRun.getCTR().getDrawingList().clear();
+                                for (XWPFPicture picture : run.getEmbeddedPictures()) {
+                                    byte[] data = picture.getPictureData().getData();
+                                    newRun.addPicture(new ByteArrayInputStream(data), picture.getPictureData().getPictureType(),
+                                            picture.getPictureData().getFileName(), (int) picture.getCTPicture().getSpPr().getXfrm().getExt().getCx(),
+                                            (int) picture.getCTPicture().getSpPr().getXfrm().getExt().getCy());
+                                }
+                                picturesEmbedded = true;
                             }
-                            newRun.getEmbeddedPictures().clear();
-                            newRun.getCTR().getDrawingList().clear();
-                            for (XWPFPicture picture : run.getEmbeddedPictures()) {
-                                byte[] data = picture.getPictureData().getData();
-                                newRun.addPicture(new ByteArrayInputStream(data), picture.getPictureData().getPictureType(),
-                                        picture.getPictureData().getFileName(), (int) picture.getCTPicture().getSpPr().getXfrm().getExt().getCx(),
-                                        (int) picture.getCTPicture().getSpPr().getXfrm().getExt().getCy());
-                            }
+                        }
+                        if (!picturesEmbedded) {
+                            newParagraph.getCTP().set(paragraph.getCTP().copy());
+                        }
+                        if (mergingNumbering != null && newParagraph.getNumID() != null) {
+                            newParagraph.setNumID(num.getCTNum().getNumId());
                         }
                         if (docxInfo.addBreak) {
                             newParagraph.setPageBreak(true);
@@ -95,12 +106,14 @@ public class MergeDocxHandler extends OfficeFilesSupplierHandler<MergeDocxConfig
                     } else if (bodyElement instanceof XWPFTable) {
                         XmlObject xmlo = ((XWPFTable) bodyElement).getCTTbl().copy();
                         XWPFTable table = new XWPFTable((CTTbl) xmlo, document);
+                        for (CTStyle style : mergingDocument.getStyle().getStyleList()) {
+                            if (style.getStyleId().equals(table.getStyleID())) {
+                                document.getStyles().addStyle(new XWPFStyle((CTStyle) style.copy()));
+                            }
+                        }
                         document.createTable();
                         document.setTable(document.getTables().size() - 1, table);
                     }
-                }
-                for (CTStyle style : mergingDocument.getStyle().getStyleList()) {
-                    document.getStyles().addStyle(new XWPFStyle((CTStyle) style.copy()));
                 }
             }
         }
