320515b57a634316fd0df4edf98f66464383e643
Mikhail Alekseev <malekseev@processtech.ru>
2020-09-30 12:37:54 +0300

    #1851 Использование более свежей версии диалекта

--- wfe-core/src/main/java/ru/runa/wfe/commons/hibernate/SqlServerUnicodeDialect.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/hibernate/SqlServerUnicodeDialect.java
@@ -1,16 +1,15 @@
 package ru.runa.wfe.commons.hibernate;
 
 import java.sql.Types;
-
-import org.hibernate.dialect.SQLServerDialect;
+import org.hibernate.dialect.SQLServer2012Dialect;
 
 /**
  * Maps strings to nvarchar's and unicode support for other types in MSSQL server.
- * 
+ *
  * @author dofs
  * @since 3.5
  */
-public class SqlServerUnicodeDialect extends SQLServerDialect {
+public class SqlServerUnicodeDialect extends SQLServer2012Dialect {
 
     public SqlServerUnicodeDialect() {
         registerColumnType(Types.CHAR, "nchar(1)");
