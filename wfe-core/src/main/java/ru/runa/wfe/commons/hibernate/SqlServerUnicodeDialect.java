09c4bd631331a0f834aa631355c85b0336270279
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-14 23:35:34 +0300

    #1851 Правки nvarchar в диалекте

--- wfe-core/src/main/java/ru/runa/wfe/commons/hibernate/SqlServerUnicodeDialect.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/hibernate/SqlServerUnicodeDialect.java
@@ -10,11 +10,23 @@ import org.hibernate.dialect.SQLServer2012Dialect;
  * @since 3.5
  */
 public class SqlServerUnicodeDialect extends SQLServer2012Dialect {
+    /**
+     * {@link org.hibernate.dialect.SQLServer2005Dialect#MAX_LENGTH}
+     */
+    public static final int MAX_LENGTH = 8000;
+
 
     public SqlServerUnicodeDialect() {
         registerColumnType(Types.CHAR, "nchar(1)");
-        registerColumnType(Types.VARCHAR, "nvarchar($l)");
-        registerColumnType(Types.LONGVARCHAR, "nvarchar($l)");
-        registerColumnType(Types.CLOB, "ntext");
+        registerColumnType(Types.LONGVARCHAR, "nvarchar(MAX)");
+        registerColumnType(Types.CLOB, "nvarchar(MAX)");
+
+        /*
+        Types.VARCHAR -> nvarchar(MAX), Types.VARCHAR(MAX) -> nvarchar($l) is important and not a mistake
+        because of hibernate type resolving.
+        See SQLServer2005Dialect constructor
+         */
+        registerColumnType(Types.VARCHAR, "nvarchar(MAX)");
+        registerColumnType(Types.VARCHAR, MAX_LENGTH, "nvarchar($l)");
     }
 }
