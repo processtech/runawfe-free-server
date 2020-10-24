46ea3875c6ad97c1001d8840e74451f7f929a520
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-24 14:07:58 +0300

    #1912 Адаптация CreateAdminScriptTables из develop

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/CreateAdminScriptTables.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/CreateAdminScriptTables.java
@@ -1,45 +1,18 @@
 package ru.runa.wfe.commons.dbmigration.impl;
 
-import java.sql.Types;
-import java.util.LinkedList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import ru.runa.wfe.commons.dbmigration.DbMigration;
-import ru.runa.wfe.script.AdminScript;
-import ru.runa.wfe.security.dao.PermissionDao;
-import ru.runa.wfe.user.dao.ExecutorDao;
 
 public class CreateAdminScriptTables extends DbMigration {
-
-    @Autowired
-    protected ExecutorDao executorDao;
-    @Autowired
-    protected PermissionDao permissionDao;
-
     @Override
-    protected List<String> getDDLQueriesBefore() {
-        List<String> sql = super.getDDLQueriesBefore();
-        sql.addAll(createAdminScriptTable());
-        return sql;
+    protected void executeDDLBefore() {
+        executeUpdates(
+                getDDLCreateTable("ADMIN_SCRIPT", list(
+                        new BigintColumnDef("ID").primaryKey(),
+                        new VarcharColumnDef("NAME", 1024).notNull(),
+                        new BlobColumnDef("CONTENT").notNull()
+                )),
+                getDDLCreateSequence("SEQ_ADMIN_SCRIPT"),
+                getDDLCreateUniqueKey("ADMIN_SCRIPT", "IX_ADMIN_SCRIPT_NAME", "NAME")
+        );
     }
-
-    /**
-     * Creates table, indexes e.t.c for {@link AdminScript}.
-     * 
-     * @return Returns list of sql commands for table creation.
-     */
-    private List<String> createAdminScriptTable() {
-        List<String> sql = new LinkedList<String>();
-        List<ColumnDef> columns = new LinkedList<DbMigration.ColumnDef>();
-        columns.add(new ColumnDef("ID", Types.BIGINT, false).setPrimaryKey());
-        columns.add(new ColumnDef("NAME", dialect.getTypeName(Types.VARCHAR, 1024, 1024, 1024), false));
-        columns.add(new ColumnDef("CONTENT", dialect.getTypeName(Types.BLOB), false));
-        sql.add(getDDLCreateTable("ADMIN_SCRIPT", columns, null));
-        sql.add(getDDLCreateSequence("SEQ_ADMIN_SCRIPT"));
-        sql.add(getDDLCreateUniqueKey("ADMIN_SCRIPT", "IX_ADMIN_SCRIPT_NAME", "NAME"));
-        return sql;
-    }
-
 }
