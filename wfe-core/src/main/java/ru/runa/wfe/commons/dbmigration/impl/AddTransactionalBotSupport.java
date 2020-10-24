2d77fd2a4c8534265264ef14e39abebc061d9ebd
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-24 14:06:14 +0300

    #1912 Адаптация AddTransactionalBotSupport из develop

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddTransactionalBotSupport.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddTransactionalBotSupport.java
@@ -1,21 +1,19 @@
 package ru.runa.wfe.commons.dbmigration.impl;
 
 import java.sql.Types;
-import java.util.List;
 import ru.runa.wfe.commons.dbmigration.DbMigration;
 
 public class AddTransactionalBotSupport extends DbMigration {
     private static final String TABLE_NAME = "BOT";
 
     @Override
-    protected List<String> getDDLQueriesBefore() {
-        List<String> sql = super.getDDLQueriesAfter();
-        sql.add(getDDLCreateColumn(TABLE_NAME, new ColumnDef("IS_TRANSACTIONAL", dialect.getTypeName(Types.BIT))));
-        sql.add(getDDLCreateColumn(TABLE_NAME, new ColumnDef("TRANSACTIONAL_TIMEOUT", dialect.getTypeName(Types.BIGINT))));
-        sql.add(getDDLCreateColumn(TABLE_NAME, new ColumnDef("BOUND_DUE_DATE", dialect.getTypeName(Types.TIMESTAMP))));
-        sql.add(getDDLCreateColumn(TABLE_NAME, new ColumnDef("BOUND_PROCESS_ID", dialect.getTypeName(Types.BIGINT))));
-        sql.add(getDDLCreateColumn(TABLE_NAME, new ColumnDef("BOUND_SUBPROCESS_ID", dialect.getTypeName(Types.VARCHAR, 1024, 1024, 1024))));
-        return sql;
+    protected void executeDDLBefore() {
+        executeUpdates(
+                getDDLCreateColumn(TABLE_NAME, new BooleanColumnDef("IS_TRANSACTIONAL")),
+                getDDLCreateColumn(TABLE_NAME, new BigintColumnDef("TRANSACTIONAL_TIMEOUT")),
+                getDDLCreateColumn(TABLE_NAME, new ColumnDef("BOUND_DUE_DATE", dialect.getTypeName(Types.TIMESTAMP))),
+                getDDLCreateColumn(TABLE_NAME, new BigintColumnDef("BOUND_PROCESS_ID")),
+                getDDLCreateColumn(TABLE_NAME, new VarcharColumnDef("BOUND_SUBPROCESS_ID", 1024))
+        );
     }
-
 }
