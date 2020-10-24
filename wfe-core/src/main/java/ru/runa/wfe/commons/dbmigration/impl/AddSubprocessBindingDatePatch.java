3dc238246b8cef9fa637cc0957f5de6092d757ea
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-24 13:46:44 +0300

    #1912 Адаптация AddSubprocessBindingDatePatch из develop

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddSubprocessBindingDatePatch.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddSubprocessBindingDatePatch.java
@@ -1,22 +1,12 @@
 package ru.runa.wfe.commons.dbmigration.impl;
 
 import java.sql.Types;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import ru.runa.wfe.audit.dao.ProcessLogDao;
 import ru.runa.wfe.commons.dbmigration.DbMigration;
 
 public class AddSubprocessBindingDatePatch extends DbMigration {
-    @Autowired
-    private ProcessLogDao processLogDao;
 
     @Override
-    protected List<String> getDDLQueriesBefore() {
-        List<String> sql = super.getDDLQueriesBefore();
-        sql.add(getDDLCreateColumn("BPM_PROCESS_DEFINITION", new ColumnDef("SUBPROCESS_BINDING_DATE", dialect.getTypeName(Types.TIMESTAMP))));
-        return sql;
+    protected void executeDDLBefore() {
+        executeUpdates(getDDLCreateColumn("BPM_PROCESS_DEFINITION", new ColumnDef("SUBPROCESS_BINDING_DATE", dialect.getTypeName(Types.TIMESTAMP))));
     }
-
 }
