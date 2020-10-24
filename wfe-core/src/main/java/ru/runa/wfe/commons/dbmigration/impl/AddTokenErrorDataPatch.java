09e2f85f2483c7ee4b7cae4da4077dabcab09df3
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-24 13:48:06 +0300

    #1912 Адаптация AddTokenErrorDataPatch из develop

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddTokenErrorDataPatch.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddTokenErrorDataPatch.java
@@ -1,18 +1,14 @@
 package ru.runa.wfe.commons.dbmigration.impl;
 
-import java.sql.Types;
-import java.util.List;
-
 import ru.runa.wfe.commons.dbmigration.DbMigration;
 
 public class AddTokenErrorDataPatch extends DbMigration {
 
     @Override
-    protected List<String> getDDLQueriesBefore() {
-        List<String> sql = super.getDDLQueriesBefore();
-        sql.add(getDDLCreateColumn("BPM_TOKEN", new ColumnDef("ERROR_DATE", Types.TIMESTAMP)));
-        sql.add(getDDLCreateColumn("BPM_TOKEN", new ColumnDef("ERROR_MESSAGE", dialect.getTypeName(Types.VARCHAR, 1024, 1024, 1024))));
-        return sql;
+    protected void executeDDLBefore() {
+        executeUpdates(
+                getDDLCreateColumn("BPM_TOKEN", new TimestampColumnDef("ERROR_DATE")),
+                getDDLCreateColumn("BPM_TOKEN", new VarcharColumnDef("ERROR_MESSAGE", 1024))
+        );
     }
-
 }
