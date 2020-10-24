8e7cd0a9709deb1f62b2eb74a0553b3c4ecdeaa0
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-24 13:46:06 +0300

    #1912 Адаптация AddSequentialFlagToBot из develop

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddSettingsTable.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddSettingsTable.java
@@ -1,26 +1,19 @@
 package ru.runa.wfe.commons.dbmigration.impl;
 
-import java.sql.Types;
-import java.util.LinkedList;
-import java.util.List;
-
 import ru.runa.wfe.commons.dbmigration.DbMigration;
 
 public class AddSettingsTable extends DbMigration {
 
     @Override
-    protected List<String> getDDLQueriesBefore() {
-        List<String> sql = super.getDDLQueriesAfter();
-        List<ColumnDef> columns = new LinkedList<DbMigration.ColumnDef>();
-        ColumnDef id = new ColumnDef("ID", Types.BIGINT, false);
-        id.setPrimaryKey();
-        columns.add(id);
-        columns.add(new ColumnDef("FILE_NAME", dialect.getTypeName(Types.VARCHAR, 1024, 1024, 1024), false));
-        columns.add(new ColumnDef("NAME", dialect.getTypeName(Types.VARCHAR, 1024, 1024, 1024), false));
-        columns.add(new ColumnDef("VALUE", dialect.getTypeName(Types.VARCHAR, 1024, 1024, 1024), true));
-        sql.add(getDDLCreateTable("BPM_SETTING", columns, null));
-        sql.add(getDDLCreateSequence("SEQ_BPM_SETTING"));
-        return sql;
+    protected void executeDDLBefore() {
+        executeUpdates(
+                getDDLCreateTable("BPM_SETTING", list(
+                        new BigintColumnDef("ID").notNull().primaryKey(),
+                        new VarcharColumnDef("FILE_NAME", 1024).notNull(),
+                        new VarcharColumnDef("NAME", 1024).notNull(),
+                        new VarcharColumnDef("VALUE", 1024).notNull()
+                )),
+                getDDLCreateSequence("SEQ_BPM_SETTING")
+        );
     }
-
 }
