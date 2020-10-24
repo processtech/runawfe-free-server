07b03a3f3c69f8e4962c1f05e00d2d5c1f138f66
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-24 13:44:13 +0300

    #1912 Адаптация AddProcessExternalData из develop

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddProcessExternalData.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddProcessExternalData.java
@@ -1,16 +1,14 @@
 package ru.runa.wfe.commons.dbmigration.impl;
 
-import java.util.ArrayList;
-import java.util.List;
 import ru.runa.wfe.commons.dbmigration.DbMigration;
 
 public class AddProcessExternalData extends DbMigration {
 
     @Override
-    protected List<String> getDDLQueriesBefore() {
-        return new ArrayList<String>() {{
-            add(getDDLCreateColumn("bpm_process", new BigintColumnDef("external_data", true)));
-            add(getDDLCreateIndex("bpm_process", "ix_process_external_data", "external_data"));
-        }};
+    protected void executeDDLBefore() {
+        executeUpdates(
+            getDDLCreateColumn("bpm_process", new BigintColumnDef("external_data")),
+            getDDLCreateIndex("bpm_process", "ix_process_external_data", "external_data")
+        );
     }
 }
