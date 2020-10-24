5e5359243fc901bca31e303c34a2a6a23a28f984
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-24 14:24:01 +0300

    #1912 Адаптация TaskOpenedByExecutorsPatch из develop

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/TaskOpenedByExecutorsPatch.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/TaskOpenedByExecutorsPatch.java
@@ -1,23 +1,17 @@
 package ru.runa.wfe.commons.dbmigration.impl;
 
-import java.sql.Types;
-import java.util.List;
-
 import org.hibernate.ScrollMode;
 import org.hibernate.ScrollableResults;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import ru.runa.wfe.commons.dbmigration.DbMigration;
 import ru.runa.wfe.user.Actor;
 import ru.runa.wfe.user.Executor;
 import ru.runa.wfe.user.dao.ExecutorDao;
 
-import com.google.common.collect.Lists;
-
 /**
  * https://sourceforge.net/p/runawfe/bugs/378/
- * 
+ *
  * @author Dofs
  * @since 4.1.0
  */
@@ -27,32 +21,30 @@ public class TaskOpenedByExecutorsPatch extends DbMigration {
     private ExecutorDao executorDao;
 
     @Override
-    protected List<String> getDDLQueriesAfter() {
-        List<String> sql = super.getDDLQueriesAfter();
-        sql.add(getDDLRemoveColumn("BPM_TASK", "FIRST_OPEN"));
-        return sql;
+    protected void executeDDLAfter() {
+        executeUpdates(getDDLDropColumn("BPM_TASK", "FIRST_OPEN"));
     }
 
     @Override
-    protected List<String> getDDLQueriesBefore() {
-        List<String> sql = super.getDDLQueriesAfter();
-        List<ColumnDef> columns = Lists.newArrayList();
-        columns.add(new ColumnDef("TASK_ID", Types.BIGINT, false));
-        columns.add(new ColumnDef("EXECUTOR_ID", Types.BIGINT, false));
-        sql.add(getDDLCreateTable("BPM_TASK_OPENED", columns, null));
-        sql.add(getDDLCreateForeignKey("BPM_TASK_OPENED", "FK_TASK_OPENED_TASK", "TASK_ID", "BPM_TASK", "ID"));
-        return sql;
+    protected void executeDDLBefore() {
+        executeUpdates(
+                getDDLCreateTable("BPM_TASK_OPENED", list(
+                        new BigintColumnDef("TASK_ID").notNull(),
+                        new BigintColumnDef("EXECUTOR_ID").notNull()
+                )),
+                getDDLCreateForeignKey("BPM_TASK_OPENED", "FK_TASK_OPENED_TASK", "TASK_ID", "BPM_TASK", "ID")
+        );
     }
 
     @Override
-    public void executeDML(Session session) throws Exception {
+    public void executeDML(Session session) {
         String q;
         log.info("Processing opened tasks");
         q = "SELECT ID, EXECUTOR_ID FROM BPM_TASK WHERE FIRST_OPEN=0";
         ScrollableResults scrollableResults = session.createSQLQuery(q).scroll(ScrollMode.FORWARD_ONLY);
         int processed = 0;
         while (scrollableResults.next()) {
-            Long taskId = ((Number) scrollableResults.get(0)).longValue();
+            long taskId = ((Number) scrollableResults.get(0)).longValue();
             try {
                 Long executorId = ((Number) scrollableResults.get(1)).longValue();
                 Executor executor = executorDao.getExecutor(executorId);
@@ -67,5 +59,4 @@ public class TaskOpenedByExecutorsPatch extends DbMigration {
         }
         log.info("Reverted opened tasks result: " + processed);
     }
-
 }
