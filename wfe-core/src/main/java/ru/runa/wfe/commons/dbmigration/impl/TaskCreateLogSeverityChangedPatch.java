d825a04bcb968ce96076793f701552a0b00128e1
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-24 14:22:32 +0300

    #1912 Адаптация TaskCreateLogSeverityChangedPatch из develop

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/TaskCreateLogSeverityChangedPatch.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/TaskCreateLogSeverityChangedPatch.java
@@ -2,14 +2,13 @@ package ru.runa.wfe.commons.dbmigration.impl;
 
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
-
 import ru.runa.wfe.audit.Severity;
 import ru.runa.wfe.commons.dbmigration.DbMigration;
 
 public class TaskCreateLogSeverityChangedPatch extends DbMigration {
 
     @Override
-    public void executeDML(Session session) throws Exception {
+    public void executeDML(Session session) {
         SQLQuery updateQuery = session.createSQLQuery("UPDATE BPM_LOG SET SEVERITY=:severity WHERE DISCRIMINATOR='1'");
         updateQuery.setString("severity", Severity.INFO.name());
         updateQuery.executeUpdate();
