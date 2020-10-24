334b81fb91525f577071b32b0283d1467b87ecee
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-24 14:20:39 +0300

    #1912 Адаптация PermissionMappingPatch403 из develop

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/PermissionMappingPatch403.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/PermissionMappingPatch403.java
@@ -1,19 +1,14 @@
 package ru.runa.wfe.commons.dbmigration.impl;
 
-import java.util.List;
-
 import ru.runa.wfe.commons.dbmigration.DbMigration;
-
-import com.google.common.collect.Lists;
 
 public class PermissionMappingPatch403 extends DbMigration {
 
     @Override
-    protected List<String> getDDLQueriesAfter() {
-        List<String> sql = Lists.newArrayList();
-        sql.add(getDDLRemoveIndex("PERMISSION_MAPPING", "IX_PERMISSION_BY_IDENTIFIABLE"));
-        sql.add(getDDLCreateUniqueKey("PERMISSION_MAPPING", "UQ_MAPPINGS", "IDENTIFIABLE_ID", "TYPE_ID", "MASK", "EXECUTOR_ID"));
-        return sql;
+    protected void executeDDLAfter() {
+        executeUpdates(
+                getDDLDropIndex("PERMISSION_MAPPING", "IX_PERMISSION_BY_IDENTIFIABLE"),
+                getDDLCreateUniqueKey("PERMISSION_MAPPING", "UQ_MAPPINGS", "IDENTIFIABLE_ID", "TYPE_ID", "MASK", "EXECUTOR_ID")
+        );
     }
-
 }
