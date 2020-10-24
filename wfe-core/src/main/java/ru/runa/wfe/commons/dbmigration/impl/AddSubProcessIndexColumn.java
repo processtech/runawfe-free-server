77fe736c75e289c7337d6e83cb1b945ca1c2786f
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-24 13:47:13 +0300

    #1912 Адаптация AddSubProcessIndexColumn из develop

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddSubProcessIndexColumn.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddSubProcessIndexColumn.java
@@ -1,17 +1,11 @@
 package ru.runa.wfe.commons.dbmigration.impl;
 
-import java.sql.Types;
-import java.util.List;
-
 import ru.runa.wfe.commons.dbmigration.DbMigration;
 
 public class AddSubProcessIndexColumn extends DbMigration {
 
     @Override
-    protected List<String> getDDLQueriesBefore() {
-        List<String> sql = super.getDDLQueriesBefore();
-        sql.add(getDDLCreateColumn("BPM_SUBPROCESS", new ColumnDef("SUBPROCESS_INDEX", dialect.getTypeName(Types.INTEGER))));
-        return sql;
+    protected void executeDDLBefore() {
+        executeUpdates(getDDLCreateColumn("BPM_SUBPROCESS", new IntColumnDef("SUBPROCESS_INDEX")));
     }
-
 }
