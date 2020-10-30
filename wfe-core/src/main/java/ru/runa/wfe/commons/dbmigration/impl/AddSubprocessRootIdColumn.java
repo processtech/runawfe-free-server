d1f51dafeb283f5e985ac35aee0adfad0865f43e
Mikhail Alekseev <malekseev@processtech.ru>
2020-10-30 18:04:07 +0300

    #1882 Игнорируем dml для mysql

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddSubprocessRootIdColumn.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddSubprocessRootIdColumn.java
@@ -21,41 +21,36 @@ public class AddSubprocessRootIdColumn extends DbMigration {
     @Override
     public void executeDML(Connection conn) {
         DbType dbType = ApplicationContextFactory.getDbType();
+        if (dbType == DbType.MYSQL) {
+            return;
+        }
         String sql;
 
         // Set ROOT_PROCESS_ID for all first-level subprocesses.
-        switch (dbType) {
-            default:
-                // They say this is standard syntax: https://stackoverflow.com/a/31410053/4247442; however had to remove alias
-                // from outer table because looks like MS SQL may not support it: https://stackoverflow.com/a/14618713/4247442.
-                sql = "update bpm_subprocess " +
-                        "set root_process_id = parent_process_id " +
-                        "where NOT exists (" +
-                        // Subquery returns non-empty rowset if bpm_subprocess.parent_process_id is someone else's child (i.e. non-root).
-                        // I select same s2.process_id field which is used in where clause, so only index ix_subprocess_process is accessed
-                        // (although good SQL server should perform this optimization anyway).
-                        "    select s2.process_id " +
-                        "    from bpm_subprocess s2 " +
-                        "    where s2.process_id = bpm_subprocess.parent_process_id " +
-                        ")";
-                break;
-        }
+        // They say this is standard syntax: https://stackoverflow.com/a/31410053/4247442; however had to remove alias
+        // from outer table because looks like MS SQL may not support it: https://stackoverflow.com/a/14618713/4247442.
+        sql = "update bpm_subprocess " +
+                "set root_process_id = parent_process_id " +
+                "where NOT exists (" +
+                // Subquery returns non-empty rowset if bpm_subprocess.parent_process_id is someone else's child (i.e. non-root).
+                // I select same s2.process_id field which is used in where clause, so only index ix_subprocess_process is accessed
+                // (although good SQL server should perform this optimization anyway).
+                "    select s2.process_id " +
+                "    from bpm_subprocess s2 " +
+                "    where s2.process_id = bpm_subprocess.parent_process_id " +
+                ")";
         executeUpdates(sql);
 
         // Set ROOT_PROCESS_ID in loop (each step -- next deepness level in all trees), until nothing more to do.
         do {
-            switch (dbType) {
-                default:
-                    // Subquery selects parent subprocess which has ROOT_PROCESS_ID already set.
-                    // It's used in "WHERE EXISTS" too, since otherwise it will be evaulated to NULL in "SET".
-                    String subquery = "select s2.root_process_id " +
-                            "from bpm_subprocess s2 " +
-                            "where (s2.process_id = bpm_subprocess.parent_process_id) and (s2.root_process_id is not null)";
-                    sql = "update bpm_subprocess " +
-                            "set root_process_id = (" + subquery + ") " +
-                            "where root_process_id is null and exists (" + subquery + ")";
-                    break;
-            }
+            // Subquery selects parent subprocess which has ROOT_PROCESS_ID already set.
+            // It's used in "WHERE EXISTS" too, since otherwise it will be evaulated to NULL in "SET".
+            String subquery = "select s2.root_process_id " +
+                    "from bpm_subprocess s2 " +
+                    "where (s2.process_id = bpm_subprocess.parent_process_id) and (s2.root_process_id is not null)";
+            sql = "update bpm_subprocess " +
+                    "set root_process_id = (" + subquery + ") " +
+                    "where root_process_id is null and exists (" + subquery + ")";
         } while (executeUpdates(sql) != 0);
     }
 
