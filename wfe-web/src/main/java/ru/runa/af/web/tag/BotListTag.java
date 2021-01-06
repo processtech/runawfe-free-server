1fd638e072c017889fa507cad8c17c64fe5fec19
Khramov Aleksey <hramovalexey@yandex.ru>
2021-01-06 22:26:27 +0500

    trying to correct formatting

--- wfe-web/src/main/java/ru/runa/af/web/tag/BotListTag.java
+++ wfe-web/src/main/java/ru/runa/af/web/tag/BotListTag.java
@@ -23,76 +23,76 @@ import ru.runa.wfe.service.delegate.Delegates;
 @org.tldgen.annotations.Tag(bodyContent = BodyContent.JSP, name = "botListTag")
 public class BotListTag extends TitledFormTag {
 
-  private static final long serialVersionUID = 1L;
-  private Long botStationId;
+    private static final long serialVersionUID = 1L;
+    private Long botStationId;
 
-  public Long getBotStationId() {
-    return botStationId;
-  }
+    public Long getBotStationId() {
+        return botStationId;
+    }
 
-  @Attribute(required = false, rtexprvalue = true)
-  public void setBotStationId(Long botStationId) {
-    this.botStationId = botStationId;
-  }
+    @Attribute(required = false, rtexprvalue = true)
+    public void setBotStationId(Long botStationId) {
+        this.botStationId = botStationId;
+    }
 
-  @Override
-  protected void fillFormElement(TD tdFormElement) {
-    tdFormElement.addElement(new Input(Input.hidden, IdsForm.ID_INPUT_NAME, Long.toString(botStationId)));
-    List<Bot> bots = Delegates.getBotService().getBots(getUser(), botStationId);
-    tdFormElement.addElement(new BotTableBuilder(pageContext).buildBotTable(bots));
-  }
+    @Override
+    protected void fillFormElement(TD tdFormElement) {
+        tdFormElement.addElement(new Input(Input.hidden, IdsForm.ID_INPUT_NAME, Long.toString(botStationId)));
+        List<Bot> bots = Delegates.getBotService().getBots(getUser(), botStationId);
+        tdFormElement.addElement(new BotTableBuilder(pageContext).buildBotTable(bots));
+    }
 
-  @Override
-  protected String getTitle() {
-    return MessagesBot.TITLE_BOT_LIST.message(pageContext);
-  }
+    @Override
+    protected String getTitle() {
+        return MessagesBot.TITLE_BOT_LIST.message(pageContext);
+    }
 
-  @Override
-  protected String getSubmitButtonName() {
-    return MessagesCommon.BUTTON_REMOVE.message(pageContext);
-  }
+    @Override
+    protected String getSubmitButtonName() {
+        return MessagesCommon.BUTTON_REMOVE.message(pageContext);
+    }
 
-  @Override
-  protected boolean isMultipleSubmit() {
-    return true;
-  }
+    @Override
+    protected boolean isMultipleSubmit() {
+        return true;
+    }
 
-  @Override
-  protected List<Map<String, String>> getSubmitButtonsData() {
-    List<Map<String, String>> data = Lists.newArrayList();
-    Map<String, String> buttonData1 = Maps.newHashMap();
-    buttonData1.put("name", getSubmitButtonName());
-    buttonData1.put("color", "");
-    buttonData1.put("type", Input.SUBMIT);
-    buttonData1.put("style", "float: right;");
-    buttonData1.put("onclick", ConfirmationPopupHelper.getInstance().getConfirmationPopupCodeHTML(getConfirmationPopupParameter(), pageContext));
-    Map<String, String> buttonData2 = Maps.newHashMap();
-    buttonData2.put("name", MessagesCommon.BUTTON_CREATE.message(pageContext));
-    buttonData2.put("color", "");
-    buttonData2.put("type", Input.BUTTON);
-    buttonData2.put("style", "float: left;");
-    buttonData2.put("onclick", getCreateAction());
-    data.add(buttonData1);
-    data.add(buttonData2);
-    return data;
-  }
+    @Override
+    protected List<Map<String, String>> getSubmitButtonsData() {
+        List<Map<String, String>> data = Lists.newArrayList();
+        Map<String, String> buttonData1 = Maps.newHashMap();
+        buttonData1.put("name", getSubmitButtonName());
+        buttonData1.put("color", "");
+        buttonData1.put("type", Input.SUBMIT);
+        buttonData1.put("style", "float: right;");
+        buttonData1.put("onclick", ConfirmationPopupHelper.getInstance().getConfirmationPopupCodeHTML(getConfirmationPopupParameter(), pageContext));
+        Map<String, String> buttonData2 = Maps.newHashMap();
+        buttonData2.put("name", MessagesCommon.BUTTON_CREATE.message(pageContext));
+        buttonData2.put("color", "");
+        buttonData2.put("type", Input.BUTTON);
+        buttonData2.put("style", "float: left;");
+        buttonData2.put("onclick", getCreateAction());
+        data.add(buttonData1);
+        data.add(buttonData2);
+        return data;
+    }
 
-  @Override
-  public String getAction() {
-    return DeleteBotAction.DELETE_BOT_ACTION_PATH;
-  }
+    @Override
+    public String getAction() {
+        return DeleteBotAction.DELETE_BOT_ACTION_PATH;
+    }
 
-  public String getCreateAction() {
-    return String.format("window.location=\"/wfe/add_bot.do?botStationId=%s\"", botStationId);
-  }
+    public String getCreateAction() {
+        return String.format("window.location=\"/wfe/add_bot.do?botStationId=%s\"", botStationId);
+    }
 
-  @Override
-  public boolean isSubmitButtonEnabled() {
-    return Delegates.getAuthorizationService().isAllowed(getUser(), Permission.UPDATE, SecuredSingleton.BOTSTATIONS);
-  }
+    @Override
+    public boolean isSubmitButtonEnabled() {
+        return Delegates.getAuthorizationService().isAllowed(getUser(), Permission.UPDATE, SecuredSingleton.BOTSTATIONS);
+    }
 
-  @Override
-  public String getConfirmationPopupParameter() {
-    return ConfirmationPopupHelper.REMOVE_BOT_PARAMETER;
-  }
+    @Override
+    public String getConfirmationPopupParameter() {
+        return ConfirmationPopupHelper.REMOVE_BOT_PARAMETER;
+    }
 }
--- wfe-web/src/main/java/ru/runa/common/web/tag/FormTag.java
+++ wfe-web/src/main/java/ru/runa/common/web/tag/FormTag.java
@@ -41,176 +41,176 @@ import ru.runa.wfe.commons.web.PortletUrlType;
  */
 abstract public class FormTag extends VisibleTag {
 
-  public static final String SUBMIT_BUTTON_NAME = "submitButton";
-  public static final String MULTIPLE_SUBMIT_BUTTONS = "multipleSubmit";
-  private static final long serialVersionUID = 1L;
-  private String action;
+    public static final String SUBMIT_BUTTON_NAME = "submitButton";
+    public static final String MULTIPLE_SUBMIT_BUTTONS = "multipleSubmit";
+    private static final long serialVersionUID = 1L;
+    private String action;
 
-  private String method = Form.POST;
+    private String method = Form.POST;
 
-  private String buttonAlignment;
-  private Form form;
+    private String buttonAlignment;
+    private Form form;
 
-  public String getAction() {
-    return action;
-  }
-
-  @Attribute
-  public void setAction(String action) {
-    this.action = action;
-  }
-
-  public String getMethod() {
-    return method;
-  }
-
-  @Attribute(required = false, rtexprvalue = false)
-  public void setMethod(String string) {
-    method = string;
-  }
-
-  public String getButtonAlignment() {
-    return buttonAlignment;
-  }
-
-  @Attribute(required = false, rtexprvalue = true)
-  public void setButtonAlignment(String buttonAlignment) {
-    this.buttonAlignment = buttonAlignment;
-  }
-
-  /**
-   * In this method descendants fill the form.
-   *
-   * @param tdFormElement
-   * @ if any exception occurred
-   */
-  abstract protected void fillFormElement(final TD tdFormElement);
-
-  /**
-   * @return returns true if form button must be displayed
-   */
-  protected boolean isSubmitButtonVisible() {
-    return true;
-  }
-
-  /**
-   * @return returns true if form button must be displayed
-   */
-  protected boolean isSubmitButtonEnabled() {
-    return true;
-  }
-
-  protected String getSubmitButtonName() {
-    return MessagesProcesses.BUTTON_FORM.message(pageContext);
-  }
-
-  protected Map<String, Object> getSubmitButtonParam() {
-    return null;
-  }
-
-  protected boolean isMultipleSubmit() {
-    return false;
-  }
-
-  protected List<Map<String, String>> getSubmitButtonsData() {
-    return null;
-  }
-
-  protected Form getForm() {
-    return form;
-  }
-
-  public boolean isConfirmationPopupEnabled() {
-    return ConfirmationPopupHelper.getInstance().isEnabled(getConfirmationPopupParameter());
-  }
-
-  protected String getConfirmationPopupParameter() {
-    return "";
-  }
-
-  protected boolean isCancelButtonEnabled() {
-    return false;
-  }
-
-  protected String getCancelButtonAction() {
-    return "";
-  }
-
-  @Override
-  protected ConcreteElement getEndElement() {
-    form = new Form();
-    Table table = new Table();
-    table.setClass(Resources.CLASS_BOX);
-    form.addElement(table);
-    TR formElementTR = new TR();
-    table.addElement(formElementTR);
-    TD formElementTD = new TD();
-    formElementTD.setClass(Resources.CLASS_BOX_BODY);
-    formElementTR.addElement(formElementTD);
-    fillFormElement(formElementTD);
-    if (getAction() != null) {
-      form.setAction(Commons.getActionUrl(getAction(), getSubmitButtonParam(), pageContext, PortletUrlType.Action));
+    public String getAction() {
+        return action;
     }
-    if (getMethod() != null) {
-      form.setMethod(getMethod());
+
+    @Attribute
+    public void setAction(String action) {
+        this.action = action;
     }
-    if (isSubmitButtonVisible()) {
-      TR tr = new TR();
-      table.addElement(tr);
-      TD td = new TD();
-      tr.addElement(td);
-      td.setClass(Resources.CLASS_BOX_BODY);
-      if (buttonAlignment != null) {
-        td.setAlign(buttonAlignment);
-      }
-      if (isMultipleSubmit()) {
-        td.addElement(new Input(Input.HIDDEN, MULTIPLE_SUBMIT_BUTTONS, "true"));
-        for (Map<String, String> buttonData : getSubmitButtonsData()) {
-          Input submitButton = new Input(buttonData.get("type"), SUBMIT_BUTTON_NAME, buttonData.get("name"));
-          String color = buttonData.get("color");
-          submitButton.setClass(Resources.CLASS_BUTTON + (Strings.isNullOrEmpty(color) ? "" : "-" + color));
-          if (buttonData.containsKey("onclick")) {
-            submitButton.addAttribute("onclick", buttonData.get("onclick"));
-          }
-          if (buttonData.containsKey("style")) {
-            submitButton.addAttribute("style", buttonData.get("style"));
-          }
-          try {
-            if (!isSubmitButtonEnabled()) {
-              submitButton.setDisabled(true);
+
+    public String getMethod() {
+        return method;
+    }
+
+    @Attribute(required = false, rtexprvalue = false)
+    public void setMethod(String string) {
+        method = string;
+    }
+
+    public String getButtonAlignment() {
+        return buttonAlignment;
+    }
+
+    @Attribute(required = false, rtexprvalue = true)
+    public void setButtonAlignment(String buttonAlignment) {
+        this.buttonAlignment = buttonAlignment;
+    }
+
+    /**
+     * In this method descendants fill the form.
+     *
+     * @param tdFormElement
+     * @ if any exception occurred
+     */
+    abstract protected void fillFormElement(final TD tdFormElement);
+
+    /**
+     * @return returns true if form button must be displayed
+     */
+    protected boolean isSubmitButtonVisible() {
+        return true;
+    }
+
+    /**
+     * @return returns true if form button must be displayed
+     */
+    protected boolean isSubmitButtonEnabled() {
+        return true;
+    }
+
+    protected String getSubmitButtonName() {
+        return MessagesProcesses.BUTTON_FORM.message(pageContext);
+    }
+
+    protected Map<String, Object> getSubmitButtonParam() {
+        return null;
+    }
+
+    protected boolean isMultipleSubmit() {
+        return false;
+    }
+
+    protected List<Map<String, String>> getSubmitButtonsData() {
+        return null;
+    }
+
+    protected Form getForm() {
+        return form;
+    }
+
+    public boolean isConfirmationPopupEnabled() {
+        return ConfirmationPopupHelper.getInstance().isEnabled(getConfirmationPopupParameter());
+    }
+
+    protected String getConfirmationPopupParameter() {
+        return "";
+    }
+
+    protected boolean isCancelButtonEnabled() {
+        return false;
+    }
+
+    protected String getCancelButtonAction() {
+        return "";
+    }
+
+    @Override
+    protected ConcreteElement getEndElement() {
+        form = new Form();
+        Table table = new Table();
+        table.setClass(Resources.CLASS_BOX);
+        form.addElement(table);
+        TR formElementTR = new TR();
+        table.addElement(formElementTR);
+        TD formElementTD = new TD();
+        formElementTD.setClass(Resources.CLASS_BOX_BODY);
+        formElementTR.addElement(formElementTD);
+        fillFormElement(formElementTD);
+        if (getAction() != null) {
+            form.setAction(Commons.getActionUrl(getAction(), getSubmitButtonParam(), pageContext, PortletUrlType.Action));
+        }
+        if (getMethod() != null) {
+            form.setMethod(getMethod());
+        }
+        if (isSubmitButtonVisible()) {
+            TR tr = new TR();
+            table.addElement(tr);
+            TD td = new TD();
+            tr.addElement(td);
+            td.setClass(Resources.CLASS_BOX_BODY);
+            if (buttonAlignment != null) {
+                td.setAlign(buttonAlignment);
             }
-          } catch (Exception e) {
-            log.debug("isSubmitButtonEnabled", e);
-            submitButton.setDisabled(true);
-          }
-          td.addElement(submitButton);
-          td.addElement(Entities.NBSP);
+            if (isMultipleSubmit()) {
+                td.addElement(new Input(Input.HIDDEN, MULTIPLE_SUBMIT_BUTTONS, "true"));
+                for (Map<String, String> buttonData : getSubmitButtonsData()) {
+                    Input submitButton = new Input(buttonData.get("type"), SUBMIT_BUTTON_NAME, buttonData.get("name"));
+                    String color = buttonData.get("color");
+                    submitButton.setClass(Resources.CLASS_BUTTON + (Strings.isNullOrEmpty(color) ? "" : "-" + color));
+                    if (buttonData.containsKey("onclick")) {
+                        submitButton.addAttribute("onclick", buttonData.get("onclick"));
+                    }
+                    if (buttonData.containsKey("style")) {
+                        submitButton.addAttribute("style", buttonData.get("style"));
+                    }
+                    try {
+                        if (!isSubmitButtonEnabled()) {
+                            submitButton.setDisabled(true);
+                        }
+                    } catch (Exception e) {
+                        log.debug("isSubmitButtonEnabled", e);
+                        submitButton.setDisabled(true);
+                    }
+                    td.addElement(submitButton);
+                    td.addElement(Entities.NBSP);
+                }
+            } else {
+                Input submitButton = new Input(Input.SUBMIT, SUBMIT_BUTTON_NAME, getSubmitButtonName());
+                submitButton.setClass(Resources.CLASS_BUTTON);
+                if (isConfirmationPopupEnabled()) {
+                    submitButton.addAttribute("onclick",
+                            ConfirmationPopupHelper.getInstance().getConfirmationPopupCodeHTML(getConfirmationPopupParameter(), pageContext));
+                }
+                if (!isSubmitButtonEnabled()) {
+                    submitButton.setDisabled(true);
+                }
+                td.addElement(submitButton);
+            }
+            if (isCancelButtonEnabled()) {
+                Input cancelButton = new Input(Input.BUTTON, SUBMIT_BUTTON_NAME, MessagesCommon.BUTTON_CANCEL.message(pageContext));
+                cancelButton.setClass(Resources.CLASS_BUTTON);
+                cancelButton.addAttribute("onclick", "window.location='" + getCancelButtonAction() + "'");
+                td.addElement(Entities.NBSP);
+                td.addElement(cancelButton);
+            }
         }
-      } else {
-        Input submitButton = new Input(Input.SUBMIT, SUBMIT_BUTTON_NAME, getSubmitButtonName());
-        submitButton.setClass(Resources.CLASS_BUTTON);
-        if (isConfirmationPopupEnabled()) {
-          submitButton.addAttribute("onclick",
-              ConfirmationPopupHelper.getInstance().getConfirmationPopupCodeHTML(getConfirmationPopupParameter(), pageContext));
-        }
-        if (!isSubmitButtonEnabled()) {
-          submitButton.setDisabled(true);
-        }
-        td.addElement(submitButton);
-      }
-      if (isCancelButtonEnabled()) {
-        Input cancelButton = new Input(Input.BUTTON, SUBMIT_BUTTON_NAME, MessagesCommon.BUTTON_CANCEL.message(pageContext));
-        cancelButton.setClass(Resources.CLASS_BUTTON);
-        cancelButton.addAttribute("onclick", "window.location='" + getCancelButtonAction() + "'");
-        td.addElement(Entities.NBSP);
-        td.addElement(cancelButton);
-      }
+        return form;
     }
-    return form;
-  }
 
-  @Override
-  protected ConcreteElement getStartElement() {
-    return new StringElement();
-  }
+    @Override
+    protected ConcreteElement getStartElement() {
+        return new StringElement();
+    }
 }
