cdd8ecf3ae399bd1183ec7a2c282fe470002a951
BigArtemka <dak.112008@gmail.com>
2021-01-29 15:04:26 +0300

    Изменен ChatSocketConfigurator

--- wfe-web/src/main/java/ru/runa/common/web/ChatSocketConfigurator.java
+++ wfe-web/src/main/java/ru/runa/common/web/ChatSocketConfigurator.java
@@ -1,15 +1,29 @@
 package ru.runa.common.web;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 import javax.websocket.HandshakeResponse;
 import javax.websocket.server.HandshakeRequest;
 import javax.websocket.server.ServerEndpointConfig;
 
+@ApplicationScoped
 public class ChatSocketConfigurator extends ServerEndpointConfig.Configurator {
+
+    @Inject
+    private ChatSocket chatSocket;
+
     @Override
     public void modifyHandshake(ServerEndpointConfig config, HandshakeRequest request, HandshakeResponse response) {
         HttpSession httpSession = (HttpSession) request.getHttpSession();
         config.getUserProperties().put(HttpSession.class.getName(), httpSession);
         config.getUserProperties().put("user", Commons.getUser(httpSession));
+    }
+
+    @Override
+    public <T> T getEndpointInstance(Class<T> endpointClass) throws InstantiationException {
+        if (endpointClass.equals(ChatSocket.class))
+            return (T) chatSocket;
+        throw new InstantiationException();
     }
 }
