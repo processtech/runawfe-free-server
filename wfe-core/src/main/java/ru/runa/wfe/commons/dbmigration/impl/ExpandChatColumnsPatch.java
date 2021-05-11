eeb8d0daac8d4fe8317da5ebc6e1a9b2db0b5617
Maxim Eremenko <mieremenko@processtech.ru>
2021-05-11 20:24:18 +0300

    Update wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/ExpandChatColumnsPatch.java

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/ExpandChatColumnsPatch.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/ExpandChatColumnsPatch.java
@@ -7,7 +7,7 @@ public class ExpandChatColumnsPatch extends DbMigration {
     @Override
     protected void executeDDLBefore() {
         executeUpdates(
-                getDDLModifyColumn("CHAT_MESSAGE", new TimestampColumnDef("CREATE_DATE")),
+                getDDLModifyColumn("CHAT_MESSAGE", new TimestampColumnDef("CREATE_DATE").notNull()),
                 getDDLModifyColumn("CHAT_MESSAGE_RECIPIENT", new TimestampColumnDef("READ_DATE"))
         );
     }
