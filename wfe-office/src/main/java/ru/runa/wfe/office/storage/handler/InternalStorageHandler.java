dc77ca9f3c92877d70258681fd5e99584893f633
MaxCheb <chebola.maxim@gmail.com>
2021-06-06 23:28:39 +0300

    Работа с ботом внутреннего хранилища

--- wfe-office/src/main/java/ru/runa/wfe/office/storage/handler/InternalStorageHandler.java
+++ wfe-office/src/main/java/ru/runa/wfe/office/storage/handler/InternalStorageHandler.java
@@ -3,6 +3,8 @@ package ru.runa.wfe.office.storage.handler;
 import com.google.common.collect.Iterables;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import ru.runa.wfe.InternalApplicationException;
 import ru.runa.wfe.datasource.DataSourceStorage;
 import ru.runa.wfe.definition.FileDataProvider;
@@ -18,12 +20,14 @@ import ru.runa.wfe.var.UserType;
 import ru.runa.wfe.var.VariableProvider;
 import ru.runa.wfe.var.dto.WfVariable;
 import ru.runa.wfe.var.format.UserTypeFormat;
+import ru.runa.wfe.datasource.DataSourceStuff;
 
 /**
  * @author Alekseev Mikhail
  * @since #1507
  */
 public class InternalStorageHandler extends OfficeFilesSupplierHandler<DataBindings> {
+	private static final Log log = LogFactory.getLog(InternalStorageHandler.class);
     @Override
     protected FilesSupplierConfigParser<DataBindings> createParser() {
         return new StorageBindingsParser();
@@ -35,10 +39,22 @@ public class InternalStorageHandler extends OfficeFilesSupplierHandler<DataBindi
         final StoreService storeService = StoreServiceFactory.create(
                 DataSourceStorage.parseDataSource(config.getInputFilePath(), variableProvider),
                 variableProvider
-        );
+        );        
+        DataBinding binding = null;
         final StoreHelper storeHelper = new StoreHelper(config, variableProvider, storeService);
-
-        final DataBinding binding = Iterables.getOnlyElement(config.getBindings());
+        
+        if (config.getInputFilePath().substring(DataSourceStuff.PATH_PREFIX_DATA_SOURCE.length()+1).equals(DataSourceStuff.INTERNAL_STORAGE_DATA_SOURCE_NAME)){
+             binding = Iterables.getOnlyElement(config.getBindings());
+        }
+        else {
+            for (DataBinding variableBinding : config.getBindings()) {
+                WfVariable variableBind = variableProvider.getVariableNotNull(variableBinding.getVariableName());
+                if (variableBind.getDefinition().getUserType() != null) {
+                    binding = variableBinding;
+                }
+            }
+        }
+        
         try {
             final ExecutionResult executionResult = execute(variableProvider, binding, storeHelper);
 
@@ -54,29 +70,24 @@ public class InternalStorageHandler extends OfficeFilesSupplierHandler<DataBindi
     }
 
     protected ExecutionResult execute(VariableProvider variableProvider, DataBinding binding, StoreHelper storeHelper) throws Exception {
-        binding.getConstraints().applyPlaceholders(variableProvider);
+    	binding.getConstraints().applyPlaceholders(variableProvider);
+        final WfVariable variable = variableProvider.getVariableNotNull(binding.getVariableName());
         switch (config.getQueryType()) {
             case INSERT: {
-                final WfVariable variable = variableProvider.getVariableNotNull(binding.getVariableName());
                 storeHelper.setVariableFormat(variable.getDefinition().getFormatNotNull());
+                log.warn("binding name" + binding.getVariableName());
                 return storeHelper.save(binding, variable);
             }
             case UPDATE: {
-                final WfVariable variable = variableProvider.getVariableNotNull(binding.getVariableName());
                 storeHelper.setVariableFormat(variable.getDefinition().getFormatNotNull());
                 return storeHelper.update(binding, variable, config.getCondition());
             }
             case SELECT: {
-                final WfVariable variable = variableProvider.getVariableNotNull(config.getOutputFileVariableName());
                 storeHelper.setVariableFormat(variable.getDefinition().getFormatNotNull());
-                return storeHelper.findByFilter(
-                        binding,
-                        variableProvider.getUserType(((OnSheetConstraints) binding.getConstraints()).getSheetName()),
-                        config.getCondition()
-                );
+                return storeHelper.findByFilter(binding, storeHelper.userType(variable), config.getCondition());
             }
             case DELETE:
-                final UserType userType = variableProvider.getUserType(((OnSheetConstraints) binding.getConstraints()).getSheetName());
+                final UserType userType = storeHelper.userType(variable);
                 storeHelper.setVariableFormat(new UserTypeFormat(userType));
                 return storeHelper.delete(binding, userType, config.getCondition());
             default:
