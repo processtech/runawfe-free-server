d6b86bde355f11f2b88fe1a73d256f9258110e54
Dofs <dofs197@gmail.com>
2022-07-15 10:42:08 +0300

    rm2719 posgresql fix

--- wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddAsyncForTaskAndSubprocess.java
+++ wfe-core/src/main/java/ru/runa/wfe/commons/dbmigration/impl/AddAsyncForTaskAndSubprocess.java
@@ -16,14 +16,31 @@ public class AddAsyncForTaskAndSubprocess extends DbMigration {
     @Override
     public void executeDML(Connection conn) throws SQLException {
         try (Statement stmt = conn.createStatement()) {
-            stmt.executeUpdate("" +
-                    "UPDATE BPM_TASK T SET T.ASYNC = CASE\n" +
-                    "    WHEN EXISTS(SELECT IT.ID FROM BPM_TASK IT INNER JOIN BPM_TOKEN TK ON IT.TOKEN_ID = TK.ID AND IT.NODE_ID <> TK.NODE_ID WHERE IT.ID = T.ID)\n" +
-                    "    THEN 1 ELSE 0 END");
-            stmt.executeUpdate("" +
-                    "UPDATE BPM_SUBPROCESS S SET S.ASYNC = CASE\n" +
-                    "    WHEN EXISTS(SELECT SP.ID FROM BPM_SUBPROCESS SP INNER JOIN BPM_TOKEN TK ON SP.PARENT_TOKEN_ID = TK.ID AND SP.PARENT_NODE_ID <> TK.NODE_ID WHERE SP.ID = S.ID)\n" +
-                    "    THEN 1 ELSE 0 END");
+            switch (dbType) {
+                case POSTGRESQL: {
+                    stmt.executeUpdate("" +
+                            "UPDATE BPM_TASK T SET ASYNC = CASE\n" +
+                            "    WHEN EXISTS(SELECT IT.ID FROM BPM_TASK IT INNER JOIN BPM_TOKEN TK ON IT.TOKEN_ID = TK.ID AND IT.NODE_ID <> TK.NODE_ID WHERE IT.ID = T.ID)\n" +
+                            "    THEN TRUE ELSE FALSE END");
+                    stmt.executeUpdate("" +
+                            "UPDATE BPM_SUBPROCESS S SET ASYNC = CASE\n" +
+                            "    WHEN EXISTS(SELECT SP.ID FROM BPM_SUBPROCESS SP INNER JOIN BPM_TOKEN TK ON SP.PARENT_TOKEN_ID = TK.ID AND SP.PARENT_NODE_ID <> TK.NODE_ID WHERE SP.ID = S.ID)\n" +
+                            "    THEN TRUE ELSE FALSE END\n" +
+                            "    WHERE PARENT_PROCESS_ID IN (SELECT ID FROM BPM_PROCESS WHERE END_DATE IS NULL)");
+                    break;
+                }
+                default: {
+                    stmt.executeUpdate("" +
+                            "UPDATE BPM_TASK T SET T.ASYNC = CASE\n" +
+                            "    WHEN EXISTS(SELECT IT.ID FROM BPM_TASK IT INNER JOIN BPM_TOKEN TK ON IT.TOKEN_ID = TK.ID AND IT.NODE_ID <> TK.NODE_ID WHERE IT.ID = T.ID)\n" +
+                            "    THEN 1 ELSE 0 END");
+                    stmt.executeUpdate("" +
+                            "UPDATE BPM_SUBPROCESS S SET S.ASYNC = CASE\n" +
+                            "    WHEN EXISTS(SELECT SP.ID FROM BPM_SUBPROCESS SP INNER JOIN BPM_TOKEN TK ON SP.PARENT_TOKEN_ID = TK.ID AND SP.PARENT_NODE_ID <> TK.NODE_ID WHERE SP.ID = S.ID)\n" +
+                            "    THEN 1 ELSE 0 END\n" +
+                            "    WHERE PARENT_PROCESS_ID IN (SELECT ID FROM BPM_PROCESS WHERE END_DATE IS NULL)");
+                }
+            }
         }
     }
 }
