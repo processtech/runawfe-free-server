e8f3c05e39a44d0ed4b7d90bff87343eb0ce0b24
kharo1963 <kharo1963@gmail.com>
2023-11-09 14:51:53 +0300

    Support #2172 По прямой ссылке всем пользователям сейчас доступна страница с отображением ошибок, возможностью их удаления и активации БП.

--- wfe-web/src/main/java/ru/runa/af/web/action/CheckSingletonPermissionAndForwardAction.java
+++ wfe-web/src/main/java/ru/runa/af/web/action/CheckSingletonPermissionAndForwardAction.java
@@ -8,6 +8,7 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import ru.runa.common.web.TabHttpSessionHelper;
 import ru.runa.common.web.action.ActionBase;
+import ru.runa.wfe.security.AuthorizationException;
 import ru.runa.wfe.security.Permission;
 import ru.runa.wfe.security.SecuredObject;
 import ru.runa.wfe.security.SecuredObjectType;
@@ -22,16 +23,19 @@ public class CheckSingletonPermissionAndForwardAction extends ActionBase {
     private static class Config {
         final Permission permission;
         final String path;
-        Config(Permission permission, String path) {
+        final boolean forAdministratorOnly;
+
+        Config(Permission permission, String path, boolean forAdministratorOnly) {
             this.permission = permission;
             this.path = path;
+            this.forAdministratorOnly = forAdministratorOnly;
         }
     }
 
     private static final String TAB_FORWARD_NAME_PARAMETER_NAME = "tabForwardName";
     private static final HashMap<SecuredObjectType, Config> configs = new HashMap<SecuredObjectType, Config>() {{
-        put(SYSTEM, new Config(Permission.READ, "/WEB-INF/af/manage_system.jsp"));
-        put(ERRORS, new Config(Permission.READ, "/WEB-INF/af/manage_errors.jsp"));
+        put(SYSTEM, new Config(Permission.READ, "/WEB-INF/af/manage_system.jsp", false));
+        put(ERRORS, new Config(Permission.READ, "/WEB-INF/af/manage_errors.jsp", true));
     }};
 
     @Override
@@ -47,6 +51,9 @@ public class CheckSingletonPermissionAndForwardAction extends ActionBase {
             Config config = configs.get(securedObjectType);
             if (config == null) {
                 throw new RuntimeException("No config for securedObjectType = " + securedObjectType);
+            }
+            if (config.forAdministratorOnly && !Delegates.getExecutorService().isAdministrator(user)) {
+                throw new AuthorizationException("Only administrator can access " + request.getRequestURI());
             }
             SecuredObject securedObject = Delegates.getAuthorizationService().findSecuredObject(securedObjectType, 0L);
             Delegates.getAuthorizationService().checkAllowed(user, config.permission, securedObject);
