b1d31abce197a256c138cfd86c5b30995c2e6640
evgeny <evgenyk.work@yandex.ru>
2025-12-17 12:29:21 +0300

    #F4431 save only diff in draft

--- wfe-web/src/main/java/ru/runa/wf/web/servlet/PostTaskFormDraftCommand.java
+++ wfe-web/src/main/java/ru/runa/wf/web/servlet/PostTaskFormDraftCommand.java
@@ -7,15 +7,19 @@ import ru.runa.wf.web.FormSubmissionUtils;
 import ru.runa.wfe.commons.web.JsonAjaxCommand;
 import ru.runa.wfe.form.Interaction;
 import ru.runa.wfe.service.TaskService;
+import ru.runa.wfe.service.client.DelegateTaskVariableProvider;
 import ru.runa.wfe.service.delegate.Delegates;
 import ru.runa.wfe.task.dto.WfTask;
 import ru.runa.wfe.user.User;
 import ru.runa.wfe.util.SerialisationUtils;
 import ru.runa.wfe.var.VariableDefinition;
+import ru.runa.wfe.var.VariableProvider;
+import ru.runa.wfe.var.dto.WfVariable;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class PostTaskFormDraftCommand extends JsonAjaxCommand {
     private static boolean ENABLED = WebResources.isProcessTaskFormDraftEnabled();
@@ -32,19 +36,35 @@ public class PostTaskFormDraftCommand extends JsonAjaxCommand {
 
         TaskService taskService = Delegates.getTaskService();
         long taskId = Long.parseLong(request.getParameter("taskId"));
-        HashMap<String, Object> variables = readVariables(user, taskId, request.getParameterMap());
+        WfTask task = Delegates.getTaskService().getTask(user, taskId);
 
-        byte[] data = SerialisationUtils.serialize(variables);
-        taskService.setTaskFormDraft(user, taskId, data);
+        HashMap<String, Object> variables = readVariables(user, task, request.getParameterMap());
+        VariableProvider variableProvider = new DelegateTaskVariableProvider(user, task);
+        HashMap<String, Object> filtered = new HashMap<>();
+
+        // Сохраняем только те что отличаются
+        for (Map.Entry<String, Object> entry : variables.entrySet()) {
+            WfVariable existing = variableProvider.getVariable(entry.getKey());
+            if (null != existing
+                    && Objects.equals(entry.getValue(), existing.getValue())) {
+                continue;
+            }
+
+            filtered.put(entry.getKey(), entry.getValue());
+        }
+
+        if (!filtered.isEmpty()) {
+            byte[] data = SerialisationUtils.serialize(filtered);
+            taskService.setTaskFormDraft(user, taskId, data);
+        }
 
         return EMPTY_JSON;
     }
 
 
-    private HashMap<String, Object> readVariables(User user, long taskId, Map<String, String[]> userInput) {
+    private HashMap<String, Object> readVariables(User user, WfTask task, Map<String, String[]> userInput) {
         HashMap<String, Object> variables = new HashMap<>();
 
-        WfTask task = Delegates.getTaskService().getTask(user, taskId);
         Interaction interaction = Delegates.getDefinitionService().getTaskNodeInteraction(user, task.getDefinitionId(), task.getNodeId());
         Map<String, String> errors = new HashMap<>();
         for (VariableDefinition variableDefinition : interaction.getVariables().values()) {
