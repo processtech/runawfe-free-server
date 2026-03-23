20ae548dd3002e74e98f3ff1553b8f6f9acb3d24
Max <maxikus101@gmail.com>
2026-03-23 13:07:06 +0500

    deleted_else_branch_and_count_type_fixed

--- wfe-core/src/main/java/ru/runa/wfe/validation/impl/UniqueStorageUserTypeAttributeValidator.java
+++ wfe-core/src/main/java/ru/runa/wfe/validation/impl/UniqueStorageUserTypeAttributeValidator.java
@@ -1,6 +1,5 @@
 package ru.runa.wfe.validation.impl;
 
-import java.util.Map;
 import org.hibernate.Session;
 import ru.runa.wfe.InternalApplicationException;
 import ru.runa.wfe.commons.ApplicationContextFactory;
@@ -18,47 +17,29 @@ public class UniqueStorageUserTypeAttributeValidator extends FieldValidator {
         }
 
         String fieldName = getFieldName();
-        String tableName;
-        String columnName;
 
-        if (fieldName.contains(UserType.DELIM)) {
-            int dotIndex = fieldName.indexOf(UserType.DELIM);
-            String parentName = fieldName.substring(0, dotIndex);
-            VariableDefinition parentVd = getVariableProvider().getParsedProcessDefinition().getVariable(parentName, false);
-
-            if (parentVd == null || parentVd.getUserType() == null) {
-                throw new InternalApplicationException("Parent variable should be a UserType for field: " + fieldName);
-            }
-
-            tableName = parentVd.getUserType().getName();
-            columnName = fieldName.substring(dotIndex + 1);
-        } else {
-            VariableDefinition vd = getVariableProvider().getParsedProcessDefinition().getVariable(fieldName, false);
-
-            if (vd == null || vd.getUserType() == null) {
-                throw new InternalApplicationException("Validator should be attached to a UserType variable: " + fieldName);
-            }
-
-            tableName = vd.getUserType().getName();
-            columnName = fieldName;
-
-            if (value instanceof Map) {
-                value = ((Map<?, ?>) value).get(columnName);
-            }
+        if (!fieldName.contains(UserType.DELIM)) {
+            throw new InternalApplicationException("Validator should be attached to a UserType variable " + fieldName);
         }
 
-        if (value == null || value.toString().trim().isEmpty()) {
-            return;
+        int dotIndex = fieldName.indexOf(UserType.DELIM);
+        String parentName = fieldName.substring(0, dotIndex);
+        VariableDefinition parentVd = getVariableProvider().getParsedProcessDefinition().getVariable(parentName, false);
+
+        if (parentVd == null || parentVd.getUserType() == null) {
+            throw new InternalApplicationException("Parent variable should be a UserType for field: " + fieldName);
         }
 
+        String tableName = parentVd.getUserType().getName();
+        String columnName = fieldName.substring(dotIndex + 1);
         String sql = String.format("SELECT COUNT(*) FROM %s WHERE %s = :val", tableName, columnName);
 
         Session session = ApplicationContextFactory.getSessionFactory().getCurrentSession();
-        int count = (int) session.createSQLQuery(sql)
-                .setParameter("val", value.toString().trim())
+        Number count = (Number) session.createSQLQuery(sql)
+                .setParameter("val", value)
                 .uniqueResult();
 
-        if (count > 0) {
+        if (count.longValue() > 0) {
             addError();
         }
     }
