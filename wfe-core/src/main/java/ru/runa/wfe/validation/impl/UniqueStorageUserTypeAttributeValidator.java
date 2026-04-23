b99d3e9efb3cecdf7e87ac7fa56884ec2af08bf3
Max <maxikus101@gmail.com>
2026-04-23 14:31:55 +0500

    add excel support

--- wfe-core/src/main/java/ru/runa/wfe/validation/impl/UniqueStorageUserTypeAttributeValidator.java
+++ wfe-core/src/main/java/ru/runa/wfe/validation/impl/UniqueStorageUserTypeAttributeValidator.java
@@ -1,8 +1,29 @@
 package ru.runa.wfe.validation.impl;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.hibernate.Session;
 import ru.runa.wfe.InternalApplicationException;
 import ru.runa.wfe.commons.ApplicationContextFactory;
+import ru.runa.wfe.datasource.DataSource;
+import ru.runa.wfe.datasource.DataSourceStorage;
+import ru.runa.wfe.datasource.DataSourceStuff;
+import ru.runa.wfe.datasource.ExcelDataSource;
+import ru.runa.wfe.datasource.JdbcDataSource;
 import ru.runa.wfe.validation.FieldValidator;
 import ru.runa.wfe.var.UserType;
 import ru.runa.wfe.var.VariableDefinition;
@@ -32,15 +53,99 @@ public class UniqueStorageUserTypeAttributeValidator extends FieldValidator {
 
         String tableName = parentVd.getUserType().getName();
         String columnName = fieldName.substring(dotIndex + 1);
-        String sql = String.format("SELECT COUNT(*) FROM %s WHERE %s = :val", tableName, columnName);
+        int columnIndex = resolveAttributeColumnIndex(parentVd, columnName);
+        DataSource dataSource = DataSourceStorage.getDataSource(DataSourceStuff.INTERNAL_STORAGE_DATA_SOURCE_NAME);
 
+        boolean exists = dataSource instanceof ExcelDataSource
+                ? existsInExcelStorage((ExcelDataSource) dataSource, tableName, columnIndex, value)
+                : existsInSqlStorage(tableName, columnName, value, dataSource);
+
+        if (exists) {
+            addError();
+        }
+    }
+
+    private int resolveAttributeColumnIndex(VariableDefinition parentVd, String columnName) {
+        List<VariableDefinition> attributes = parentVd.getUserType().getAttributes();
+        for (int i = 0; i < attributes.size(); i++) {
+            if (columnName.equals(attributes.get(i).getName())) {
+                return i;
+            }
+        }
+        throw new InternalApplicationException("Attribute '" + columnName + "' is not found in user type '" + parentVd.getUserType().getName());
+    }
+
+    private boolean existsInExcelStorage(ExcelDataSource excelDataSource, String tableName, int columnIndex, Object value) throws Exception {
+        Path xlsxPath = Paths.get(excelDataSource.getFilePath(), tableName + ".xlsx");
+        Path xlsPath = Paths.get(excelDataSource.getFilePath(), tableName + ".xls");
+        Path storagePath = Files.exists(xlsxPath) ? xlsxPath : xlsPath;
+        if (!Files.exists(storagePath)) {
+            return false;
+        }
+        try (InputStream is = new FileInputStream(storagePath.toFile());
+             Workbook workbook = WorkbookFactory.create(is)) {
+            if (workbook.getNumberOfSheets() == 0) {
+                return false;
+            }
+            Sheet sheet = workbook.getSheetAt(0);
+            String normalizedInputValue = normalize(value);
+            for (Row row : sheet) {
+                Cell cell = row.getCell(columnIndex);
+                if (cell == null) {
+                    continue;
+                }
+                if (normalizedInputValue.equals(normalize(cell))) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean existsInSqlStorage(String tableName, String columnName, Object value, DataSource dataSource) throws Exception {
+        if (dataSource instanceof JdbcDataSource) {
+            String sql = String.format("SELECT COUNT(*) FROM %s WHERE %s = ?", tableName, columnName);
+            try (Connection connection = ((JdbcDataSource) dataSource).getConnection();
+                 PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setObject(1, value);
+                try (ResultSet rs = statement.executeQuery()) {
+                    if (rs.next()) {
+                        return rs.getLong(1) > 0;
+                    }
+                }
+            }
+            return false;
+        }
+        String sql = String.format("SELECT COUNT(*) FROM %s WHERE %s = :val", tableName, columnName);
         Session session = ApplicationContextFactory.getSessionFactory().getCurrentSession();
         Number count = (Number) session.createSQLQuery(sql)
                 .setParameter("val", value)
                 .uniqueResult();
+        return count != null && count.longValue() > 0;
+    }
 
-        if (count.longValue() > 0) {
-            addError();
+    private String normalize(Cell cell) {
+        switch (cell.getCellType()) {
+            case STRING:
+                return normalize(cell.getStringCellValue());
+            case NUMERIC:
+                return normalize(BigDecimal.valueOf(cell.getNumericCellValue()).stripTrailingZeros().toPlainString());
+            case BOOLEAN:
+                return normalize(Boolean.toString(cell.getBooleanCellValue()));
+            case FORMULA:
+                return normalize(cell.getCellFormula());
+            default:
+                return "";
         }
+    }
+
+    private String normalize(Object value) {
+        if (value == null) {
+            return "";
+        }
+        if (value instanceof Number) {
+            return new BigDecimal(value.toString()).stripTrailingZeros().toPlainString();
+        }
+        return value.toString().trim();
     }
 }
